716a670bc572d74bd4d1ae1f10b102434c6db8b1
Jeremy Sharpe <jsharpe@google.com>
2010-06-04 15:31:24 -0700

    Remove reflection in DatabaseMetaDataTest and replace with simply checking the static fields manually.
    Change-Id: I0e15e3d5b6c496849bb8b9f4efb95d032cf814f3

--- luni/src/test/java/org/apache/harmony/sql/tests/java/sql/DatabaseMetaDataTest.java
+++ luni/src/test/java/org/apache/harmony/sql/tests/java/sql/DatabaseMetaDataTest.java
@@ -22,9 +22,7 @@ import dalvik.annotation.TestTargets;
 import dalvik.annotation.TestLevel;
 import dalvik.annotation.TestTargetNew;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.HashMap;
+import java.sql.DatabaseMetaData;
 
 import junit.framework.TestCase;
 
@@ -41,91 +39,67 @@ public class DatabaseMetaDataTest extends TestCase {
         args = {}
     )
     public void testPublicStatics() {
-
-        HashMap<String, Number> thePublicStatics = new HashMap<String, Number>();
-        thePublicStatics.put("sqlStateSQL99", new Integer(2));
-        thePublicStatics.put("sqlStateXOpen", new Integer(1));
-        thePublicStatics.put("attributeNullableUnknown", new Short((short) 2));
-        thePublicStatics.put("attributeNullable", new Short((short) 1));
-        thePublicStatics.put("attributeNoNulls", new Short((short) 0));
-        thePublicStatics.put("tableIndexOther", new Short((short) 3));
-        thePublicStatics.put("tableIndexHashed", new Short((short) 2));
-        thePublicStatics.put("tableIndexClustered", new Short((short) 1));
-        thePublicStatics.put("tableIndexStatistic", new Short((short) 0));
-        thePublicStatics.put("typeSearchable", new Integer(3));
-        thePublicStatics.put("typePredBasic", new Integer(2));
-        thePublicStatics.put("typePredChar", new Integer(1));
-        thePublicStatics.put("typePredNone", new Integer(0));
-        thePublicStatics.put("typeNullableUnknown", new Integer(2));
-        thePublicStatics.put("typeNullable", new Integer(1));
-        thePublicStatics.put("typeNoNulls", new Integer(0));
-        thePublicStatics.put("importedKeyNotDeferrable", new Integer(7));
-        thePublicStatics.put("importedKeyInitiallyImmediate", new Integer(6));
-        thePublicStatics.put("importedKeyInitiallyDeferred", new Integer(5));
-        thePublicStatics.put("importedKeySetDefault", new Integer(4));
-        thePublicStatics.put("importedKeyNoAction", new Integer(3));
-        thePublicStatics.put("importedKeySetNull", new Integer(2));
-        thePublicStatics.put("importedKeyRestrict", new Integer(1));
-        thePublicStatics.put("importedKeyCascade", new Integer(0));
-        thePublicStatics.put("versionColumnPseudo", new Integer(2));
-        thePublicStatics.put("versionColumnNotPseudo", new Integer(1));
-        thePublicStatics.put("versionColumnUnknown", new Integer(0));
-        thePublicStatics.put("bestRowPseudo", new Integer(2));
-        thePublicStatics.put("bestRowNotPseudo", new Integer(1));
-        thePublicStatics.put("bestRowUnknown", new Integer(0));
-        thePublicStatics.put("bestRowSession", new Integer(2));
-        thePublicStatics.put("bestRowTransaction", new Integer(1));
-        thePublicStatics.put("bestRowTemporary", new Integer(0));
-        thePublicStatics.put("columnNullableUnknown", new Integer(2));
-        thePublicStatics.put("columnNullable", new Integer(1));
-        thePublicStatics.put("columnNoNulls", new Integer(0));
-        thePublicStatics.put("procedureNullableUnknown", new Integer(2));
-        thePublicStatics.put("procedureNullable", new Integer(1));
-        thePublicStatics.put("procedureNoNulls", new Integer(0));
-        thePublicStatics.put("procedureColumnResult", new Integer(3));
-        thePublicStatics.put("procedureColumnReturn", new Integer(5));
-        thePublicStatics.put("procedureColumnOut", new Integer(4));
-        thePublicStatics.put("procedureColumnInOut", new Integer(2));
-        thePublicStatics.put("procedureColumnIn", new Integer(1));
-        thePublicStatics.put("procedureColumnUnknown", new Integer(0));
-        thePublicStatics.put("procedureReturnsResult", new Integer(2));
-        thePublicStatics.put("procedureNoResult", new Integer(1));
-        thePublicStatics.put("procedureResultUnknown", new Integer(0));
-
-        Class<?> databaseMetaDataClass;
-        try {
-            databaseMetaDataClass = Class.forName("java.sql.DatabaseMetaData");
-        } catch (ClassNotFoundException e) {
-            fail("java.sql.DatabaseMetaData class not found!");
-            return;
-        } // end try
-
-        Field[] theFields = databaseMetaDataClass.getDeclaredFields();
-        int requiredModifier = Modifier.PUBLIC + Modifier.STATIC
-                + Modifier.FINAL;
-
-        int countPublicStatics = 0;
-        for (Field element : theFields) {
-            String fieldName = element.getName();
-            int theMods = element.getModifiers();
-            if (Modifier.isPublic(theMods) && Modifier.isStatic(theMods)) {
-                try {
-                    Object fieldValue = element.get(null);
-                    Object expectedValue = thePublicStatics.get(fieldName);
-                    if (expectedValue == null) {
-                        fail("Field " + fieldName + " missing!");
-                    } // end
-                    assertEquals("Field " + fieldName + " value mismatch: ",
-                            expectedValue, fieldValue);
-                    assertEquals("Field " + fieldName + " modifier mismatch: ",
-                            requiredModifier, theMods);
-                    countPublicStatics++;
-                } catch (IllegalAccessException e) {
-                    fail("Illegal access to Field " + fieldName);
-                } // end try
-            } // end if
-        } // end for
-
+        assertEquals(0, DatabaseMetaData.attributeNoNulls);
+        assertEquals(1, DatabaseMetaData.attributeNullable);
+        assertEquals(2, DatabaseMetaData.attributeNullableUnknown);
+        assertEquals(1, DatabaseMetaData.bestRowNotPseudo);
+        assertEquals(2, DatabaseMetaData.bestRowPseudo);
+        assertEquals(2, DatabaseMetaData.bestRowSession);
+        assertEquals(0, DatabaseMetaData.bestRowTemporary);
+        assertEquals(1, DatabaseMetaData.bestRowTransaction);
+        assertEquals(0, DatabaseMetaData.bestRowUnknown);
+        assertEquals(0, DatabaseMetaData.columnNoNulls);
+        assertEquals(1, DatabaseMetaData.columnNullable);
+        assertEquals(2, DatabaseMetaData.columnNullableUnknown);
+        assertEquals(1, DatabaseMetaData.functionColumnIn);
+        assertEquals(2, DatabaseMetaData.functionColumnInOut);
+        assertEquals(3, DatabaseMetaData.functionColumnOut);
+        assertEquals(5, DatabaseMetaData.functionColumnResult);
+        assertEquals(0, DatabaseMetaData.functionColumnUnknown);
+        assertEquals(0, DatabaseMetaData.functionNoNulls);
+        assertEquals(1, DatabaseMetaData.functionNoTable);
+        assertEquals(1, DatabaseMetaData.functionNullable);
+        assertEquals(2, DatabaseMetaData.functionNullableUnknown);
+        assertEquals(0, DatabaseMetaData.functionResultUnknown);
+        assertEquals(4, DatabaseMetaData.functionReturn);
+        assertEquals(2, DatabaseMetaData.functionReturnsTable);
+        assertEquals(0, DatabaseMetaData.importedKeyCascade);
+        assertEquals(5, DatabaseMetaData.importedKeyInitiallyDeferred);
+        assertEquals(6, DatabaseMetaData.importedKeyInitiallyImmediate);
+        assertEquals(3, DatabaseMetaData.importedKeyNoAction);
+        assertEquals(7, DatabaseMetaData.importedKeyNotDeferrable);
+        assertEquals(1, DatabaseMetaData.importedKeyRestrict);
+        assertEquals(4, DatabaseMetaData.importedKeySetDefault);
+        assertEquals(2, DatabaseMetaData.importedKeySetNull);
+        assertEquals(1, DatabaseMetaData.procedureColumnIn);
+        assertEquals(2, DatabaseMetaData.procedureColumnInOut);
+        assertEquals(4, DatabaseMetaData.procedureColumnOut);
+        assertEquals(3, DatabaseMetaData.procedureColumnResult);
+        assertEquals(5, DatabaseMetaData.procedureColumnReturn);
+        assertEquals(0, DatabaseMetaData.procedureColumnUnknown);
+        assertEquals(0, DatabaseMetaData.procedureNoNulls);
+        assertEquals(1, DatabaseMetaData.procedureNoResult);
+        assertEquals(1, DatabaseMetaData.procedureNullable);
+        assertEquals(2, DatabaseMetaData.procedureNullableUnknown);
+        assertEquals(0, DatabaseMetaData.procedureResultUnknown);
+        assertEquals(2, DatabaseMetaData.procedureReturnsResult);
+        assertEquals(2, DatabaseMetaData.sqlStateSQL);
+        assertEquals(2, DatabaseMetaData.sqlStateSQL99);
+        assertEquals(1, DatabaseMetaData.sqlStateXOpen);
+        assertEquals(1, DatabaseMetaData.tableIndexClustered);
+        assertEquals(2, DatabaseMetaData.tableIndexHashed);
+        assertEquals(3, DatabaseMetaData.tableIndexOther);
+        assertEquals(0, DatabaseMetaData.tableIndexStatistic);
+        assertEquals(0, DatabaseMetaData.typeNoNulls);
+        assertEquals(1, DatabaseMetaData.typeNullable);
+        assertEquals(2, DatabaseMetaData.typeNullableUnknown);
+        assertEquals(2, DatabaseMetaData.typePredBasic);
+        assertEquals(1, DatabaseMetaData.typePredChar);
+        assertEquals(0, DatabaseMetaData.typePredNone);
+        assertEquals(3, DatabaseMetaData.typeSearchable);
+        assertEquals(1, DatabaseMetaData.versionColumnNotPseudo);
+        assertEquals(2, DatabaseMetaData.versionColumnPseudo);
+        assertEquals(0, DatabaseMetaData.versionColumnUnknown);
     } // end method testPublicStatics
 
 } // end class DatabaseMetaDataTest
