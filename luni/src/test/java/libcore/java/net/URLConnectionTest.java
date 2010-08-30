f418bf447fd007cd2ec2d45b4b0399a11904e9b4
Jesse Wilson <jessewilson@google.com>
2010-08-30 13:25:32 -0700

    Support "Connection: close" from both request and response.
    This is a rewrite of fix for the same problem in AOSP master:
    https://review.source.android.com/16757
    
    This expands on that fix by supporting "Connection: close" on
    redirects by adding test coverage.
    
    Change-Id: Ia0daa9d83cf9f557b4e9a99b2c51345c98c6db14

--- luni/src/test/java/libcore/java/net/URLConnectionTest.java
+++ luni/src/test/java/libcore/java/net/URLConnectionTest.java
@@ -1148,6 +1148,57 @@ public class URLConnectionTest extends junit.framework.TestCase {
         assertEquals("ABC", new String(request.getBody(), "UTF-8"));
     }
 
+    public void testConnectionCloseInRequest() throws IOException, InterruptedException {
+        server.enqueue(new MockResponse()); // server doesn't honor the connection: close header!
+        server.enqueue(new MockResponse());
+        server.play();
+
+        HttpURLConnection a = (HttpURLConnection) server.getUrl("/").openConnection();
+        a.setRequestProperty("Connection", "close");
+        assertEquals(200, a.getResponseCode());
+
+        HttpURLConnection b = (HttpURLConnection) server.getUrl("/").openConnection();
+        assertEquals(200, b.getResponseCode());
+
+        assertEquals(0, server.takeRequest().getSequenceNumber());
+        assertEquals("When connection: close is used, each request should get its own connection",
+                0, server.takeRequest().getSequenceNumber());
+    }
+
+    public void testConnectionCloseInResponse() throws IOException, InterruptedException {
+        server.enqueue(new MockResponse().addHeader("Connection: close"));
+        server.enqueue(new MockResponse());
+        server.play();
+
+        HttpURLConnection a = (HttpURLConnection) server.getUrl("/").openConnection();
+        assertEquals(200, a.getResponseCode());
+
+        HttpURLConnection b = (HttpURLConnection) server.getUrl("/").openConnection();
+        assertEquals(200, b.getResponseCode());
+
+        assertEquals(0, server.takeRequest().getSequenceNumber());
+        assertEquals("When connection: close is used, each request should get its own connection",
+                0, server.takeRequest().getSequenceNumber());
+    }
+
+    public void testConnectionCloseWithRedirect() throws IOException, InterruptedException {
+        MockResponse response = new MockResponse()
+                .setResponseCode(HttpURLConnection.HTTP_MOVED_TEMP)
+                .addHeader("Location: /foo")
+                .addHeader("Connection: close");
+        server.enqueue(response);
+        server.enqueue(new MockResponse().setBody("This is the new location!"));
+        server.play();
+
+        URLConnection connection = server.getUrl("/").openConnection();
+        assertEquals("This is the new location!",
+                readAscii(connection.getInputStream(), Integer.MAX_VALUE));
+
+        assertEquals(0, server.takeRequest().getSequenceNumber());
+        assertEquals("When connection: close is used, each request should get its own connection",
+                0, server.takeRequest().getSequenceNumber());
+    }
+
     /**
      * Encodes the response body using GZIP and adds the corresponding header.
      */
