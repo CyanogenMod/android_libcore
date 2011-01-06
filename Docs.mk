dc3c15284f838a1330135249b7b8255b616b4670
Brian Carlstrom <bdc@google.com>
2011-01-05 17:48:05 -0800

    Add libcore-docs target
    Add a local droiddoc target that cuts the review time for javadoc
    changes from several minutes to less than a minute.
    
    Docs.mk is the list of directories to javadoc within libcore that was
    refactored from frameworks/base/Android.mk
    
    Change-Id: I5c12b48e71b849c2883ebaafaddea5f6c6dea857

---
+++ Docs.mk
@@ -0,0 +1,13 @@
+# List of libcore directories to include in documentation.
+# Shared between libcore and frameworks/base.
+
+libcore_to_document := \
+ dalvik/src/main/java/dalvik \
+ json/src/main/java \
+ junit/src/main/java \
+ luni/src/main/java/java \
+ luni/src/main/java/javax \
+ luni/src/main/java/org/xml/sax \
+ luni/src/main/java/org/w3c \
+ xml/src/main/java/org/xmlpull/v1 \
+#
