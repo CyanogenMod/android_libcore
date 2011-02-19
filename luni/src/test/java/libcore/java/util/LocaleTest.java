9bbaec8f8f415244ab80061e6637c22bf40b5ccd
Elliott Hughes <enh@google.com>
2011-02-18 17:49:44 -0800

    Cope with ICU reporting obsolete language codes.
    java.util.Locale mangles the new language codes for Hebrew and Indonesian into
    the obsolete codes. I've added the ICU aliases so that ICU knows about the
    obsolete codes (so that when we pass a mangled locale to ICU, ICU knows what
    we're talking about). This patch adds tests, and also filters out the
    duplicates caused by ICU now reporting both "he" and "iw", which we mangle
    into "iw" and "iw". I've used a LinkedHashMap to preserve ICU's ordering;
    there's no _need_ to do this, but it's nice behavior, better than the RI's.
    
    Bug: 3452611
    Change-Id: I320ede1a35c9dd5578116499d33a7172f27e95f5

--- luni/src/test/java/libcore/java/util/LocaleTest.java
+++ luni/src/test/java/libcore/java/util/LocaleTest.java
@@ -16,11 +16,19 @@
 
 package libcore.java.util;
 
+import java.text.BreakIterator;
+import java.text.Collator;
+import java.text.DateFormat;
+import java.text.DateFormatSymbols;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Locale;
 
 public class LocaleTest extends junit.framework.TestCase {
-    public void test_getDisplayName() throws Exception {
-        // http://b/2611311; if there's no display language/country/variant, use the raw codes.
+    // http://b/2611311; if there's no display language/country/variant, use the raw codes.
+    public void test_getDisplayName_raw() throws Exception {
         Locale weird = new Locale("AaBbCc", "DdEeFf", "GgHhIi");
         assertEquals("aabbcc", weird.getLanguage());
         assertEquals("", weird.getDisplayLanguage());
@@ -29,5 +37,57 @@ public class LocaleTest extends junit.framework.TestCase {
         assertEquals("GgHhIi", weird.getVariant());
         assertEquals("", weird.getDisplayVariant());
         assertEquals("aabbcc (DDEEFF,GgHhIi)", weird.getDisplayName());
+    }
+
+    public void test_getDisplayName_easy() throws Exception {
+        assertEquals("English", Locale.ENGLISH.getDisplayLanguage(Locale.ENGLISH));
+        assertEquals("German", Locale.GERMAN.getDisplayLanguage(Locale.ENGLISH));
+        assertEquals("Englisch", Locale.ENGLISH.getDisplayLanguage(Locale.GERMAN));
+        assertEquals("Deutsch", Locale.GERMAN.getDisplayLanguage(Locale.GERMAN));
+    }
+
+    // http://b/3452611; Locale.getDisplayLanguage fails for the obsolete language codes.
+    public void test_getDisplayName_obsolete() throws Exception {
+        // he (new) -> iw (obsolete)
+        assertObsolete("he", "iw", "עברית");
+        // id (new) -> in (obsolete)
+        assertObsolete("id", "in", "Bahasa Indonesia");
+    }
+
+    private static void assertObsolete(String newCode, String oldCode, String displayName) {
+        // Either code should get you the same locale.
+        Locale newLocale = new Locale(newCode);
+        Locale oldLocale = new Locale(oldCode);
+        assertEquals(newLocale, oldLocale);
+
+        // No matter what code you used to create the locale, you should get the old code back.
+        assertEquals(oldCode, newLocale.getLanguage());
+        assertEquals(oldCode, oldLocale.getLanguage());
+
+        // Check we get the right display name.
+        assertEquals(displayName, newLocale.getDisplayLanguage(newLocale));
+        assertEquals(displayName, oldLocale.getDisplayLanguage(newLocale));
+        assertEquals(displayName, newLocale.getDisplayLanguage(oldLocale));
+        assertEquals(displayName, oldLocale.getDisplayLanguage(oldLocale));
+
+        // Check that none of the 'getAvailableLocales' methods are accidentally returning two
+        // equal locales (because to ICU they're different, but we mangle one into the other).
+        assertOnce(newLocale, BreakIterator.getAvailableLocales());
+        assertOnce(newLocale, Calendar.getAvailableLocales());
+        assertOnce(newLocale, Collator.getAvailableLocales());
+        assertOnce(newLocale, DateFormat.getAvailableLocales());
+        assertOnce(newLocale, DateFormatSymbols.getAvailableLocales());
+        assertOnce(newLocale, NumberFormat.getAvailableLocales());
+        assertOnce(newLocale, Locale.getAvailableLocales());
+    }
+
+    private static void assertOnce(Locale element, Locale[] array) {
+        int count = 0;
+        for (Locale l : array) {
+            if (l.equals(element)) {
+                ++count;
+            }
+        }
+        assertEquals(1, count);
     }
 }
