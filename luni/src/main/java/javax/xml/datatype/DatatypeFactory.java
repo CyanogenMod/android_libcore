7d7379be39626cccaa84a66a82cdde8cb178bc96
Elliott Hughes <enh@google.com>
2011-02-24 19:03:29 -0800

    Fix build.
    The "new String" here was probably a deliberate attempt to avoid a public
    static final field being inlined as a constant. Revert and document.
    
    Change-Id: I864bbf82c8711e08016fd7f4f2e8a8508b95db0c

--- luni/src/main/java/javax/xml/datatype/DatatypeFactory.java
+++ luni/src/main/java/javax/xml/datatype/DatatypeFactory.java
@@ -69,7 +69,8 @@ public abstract class DatatypeFactory {
      *
      * <p>Default value is <code>org.apache.xerces.jaxp.datatype.DatatypeFactoryImpl</code>.</p>
      */
-    public static final String DATATYPEFACTORY_IMPLEMENTATION_CLASS = "org.apache.xerces.jaxp.datatype.DatatypeFactoryImpl";
+    // This uses "new String" to avoid being inlined as a constant.
+    public static final String DATATYPEFACTORY_IMPLEMENTATION_CLASS = new String("org.apache.xerces.jaxp.datatype.DatatypeFactoryImpl");
 
     /**
      * <p>Protected constructor to prevent instantiation outside of package.</p>
