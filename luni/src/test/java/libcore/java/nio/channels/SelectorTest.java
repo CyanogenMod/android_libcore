d3992b82602c2eef8e83ae7150f987cc9e0864fd
Elliott Hughes <enh@google.com>
2011-05-11 13:59:43 -0700

    Move the selector tests into one file.
    Change-Id: I95691beaf3cee3ef89f16001c1a0176da3817c54

--- luni/src/test/java/libcore/java/nio/channels/SelectorTest.java
+++ luni/src/test/java/libcore/java/nio/channels/SelectorTest.java
@@ -16,10 +16,98 @@
 package libcore.java.nio.channels;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.channels.NoConnectionPendingException;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import junit.framework.TestCase;
+import tests.net.StuckServer;
 
 public class SelectorTest extends TestCase {
+    public void testNonBlockingConnect_immediate() throws Exception {
+        // Test the case where we [probably] connect immediately.
+        Selector selector = Selector.open();
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        try {
+            ssc.configureBlocking(false);
+            ssc.socket().bind(null);
+
+            SocketChannel sc = SocketChannel.open();
+            sc.configureBlocking(false);
+            sc.connect(ssc.socket().getLocalSocketAddress());
+            SelectionKey key = sc.register(selector, SelectionKey.OP_CONNECT);
+            assertEquals(1, selector.select());
+            assertEquals(SelectionKey.OP_CONNECT, key.readyOps());
+            sc.finishConnect();
+        } finally {
+            selector.close();
+            ssc.close();
+        }
+    }
+
+    public void testNonBlockingConnect_slow() throws Exception {
+        // Test the case where we have to wait for the connection.
+        Selector selector = Selector.open();
+        StuckServer ss = new StuckServer();
+        try {
+            SocketChannel sc = SocketChannel.open();
+            sc.configureBlocking(false);
+            ss.unblockAfterMs(2000);
+            sc.connect(ss.getLocalSocketAddress());
+            SelectionKey key = sc.register(selector, SelectionKey.OP_CONNECT);
+            assertEquals(1, selector.select());
+            assertEquals(SelectionKey.OP_CONNECT, key.readyOps());
+            sc.finishConnect();
+        } finally {
+            selector.close();
+            ss.close();
+        }
+    }
+
+    // http://code.google.com/p/android/issues/detail?id=15388
+    public void testInterrupted() throws IOException {
+        Selector selector = Selector.open();
+        try {
+            Thread.currentThread().interrupt();
+            int count = selector.select();
+            assertEquals(0, count);
+        } finally {
+            selector.close();
+        }
+    }
+
+    public void testManyWakeupCallsTriggerOnlyOneWakeup() throws Exception {
+        final Selector selector = Selector.open();
+        try {
+            selector.wakeup();
+            selector.wakeup();
+            selector.wakeup();
+            selector.select();
+
+            // create a latch that will reach 0 when select returns
+            final CountDownLatch selectReturned = new CountDownLatch(1);
+            Thread thread = new Thread(new Runnable() {
+                @Override public void run() {
+                    try {
+                        selector.select();
+                        selectReturned.countDown();
+                    } catch (IOException ignored) {
+                    }
+                }
+            });
+            thread.start();
+
+            // select doesn't ever return, so await() times out and returns false
+            assertFalse(selectReturned.await(500, TimeUnit.MILLISECONDS));
+        } finally {
+            selector.close();
+        }
+    }
 
     /**
      * We previously leaked a file descriptor for each selector instance created.
