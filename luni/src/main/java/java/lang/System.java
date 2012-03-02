2e86dac8a4a2c2bbffb143cfcf9c0fb0ebc045b0
Nick Kralevich <nnk@google.com>
2012-03-02 09:36:43 -0800

    System: incorporate comments from previous review.
    Change-Id: Ieb6a4c75389dbafe832db0c3a2efdd133a062e73

--- luni/src/main/java/java/lang/System.java
+++ luni/src/main/java/java/lang/System.java
@@ -616,9 +616,8 @@ public final class System {
      *
      * <p>Security managers do <i>not</i> provide a secure environment for
      * executing untrusted code and are unsupported on Android. Untrusted code
-     * cannot be safely isolated within a single VM on Android.
-     *
-     * This method <b>always</b> throws a SecurityException.
+     * cannot be safely isolated within a single VM on Android, so this method
+     * <i>always</i> throws a {@code SecurityException}.
      *
      * @param sm a security manager
      * @throws SecurityException always
