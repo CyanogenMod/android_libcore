7c935d4e4ca990334200cf5eb4fbcfac718c6b45
gcollins <gcollins@antennasoftware.com>
2012-06-04 17:26:45 -0400

    CertificateRequest should handle case where certificate is requested but none is available.
    Android SSL client was not handling a CertificateRequest where there was no cert to send.
    It had a problem because it was assuming that if the CertificateMessage response is not null,
    it means there is a cert included, which is not true (if it has no cert to send an empty CertificateMessage
    is sent to the server). So I updated the CertificateVerify creation check to also check whether the CertificateMessage
    contained any certs (ClientHandshakeImpl.java).
    In testing I found that the same error was in the server code so I made the same change there
    (ServerHandshakeImpl.java).
    I added two test cases to SSLEngineTest - one to directly test the scenario (test_SSLEngine_clientAuthWantedNoClientCert)
    and one to just double-check that the server would not allow the connection if setNeedClientAuth (test_SSLEngine_clientAuthNeededNoClientCert).
    
    Bug: http://code.google.com/p/android/issues/detail?id=31903
    Change-Id: Ideb57d6ccbcdd54ca24dc3063e60aba2653c8414

--- luni/src/test/java/libcore/javax/net/ssl/SSLEngineTest.java
+++ luni/src/test/java/libcore/javax/net/ssl/SSLEngineTest.java
@@ -346,6 +346,51 @@ public class SSLEngineTest extends TestCase {
         c.close();
     }
 
+   /**
+    * http://code.google.com/p/android/issues/detail?id=31903
+    * This test case directly tests the fix for the issue.
+    */
+    public void test_SSLEngine_clientAuthWantedNoClientCert() throws Exception {
+        TestSSLContext clientAuthContext
+                = TestSSLContext.create(TestKeyStore.getClient(),
+                                        TestKeyStore.getServer());
+        TestSSLEnginePair p = TestSSLEnginePair.create(clientAuthContext,
+                                                       new TestSSLEnginePair.Hooks() {
+            @Override
+            void beforeBeginHandshake(SSLEngine client, SSLEngine server) {
+                server.setWantClientAuth(true);
+            }
+        });
+        assertConnected(p);
+        clientAuthContext.close();
+    }
+
+   /**
+    * http://code.google.com/p/android/issues/detail?id=31903
+    * This test case verifies that if the server requires a client cert
+    * (setNeedClientAuth) but the client does not provide one SSL connection
+    * establishment will fail
+    */
+    public void test_SSLEngine_clientAuthNeededNoClientCert() throws Exception {
+        boolean handshakeExceptionCaught = false;
+        TestSSLContext clientAuthContext
+                = TestSSLContext.create(TestKeyStore.getClient(),
+                                        TestKeyStore.getServer());
+        try {
+            TestSSLEnginePair.create(clientAuthContext,
+                             new TestSSLEnginePair.Hooks() {
+                @Override
+                void beforeBeginHandshake(SSLEngine client, SSLEngine server) {
+                    server.setNeedClientAuth(true);
+                }
+            });
+            fail();
+        } catch (SSLHandshakeException expected) {
+        } finally {
+            clientAuthContext.close();
+        }
+    }
+
     public void test_SSLEngine_getEnableSessionCreation() throws Exception {
         TestSSLContext c = TestSSLContext.create();
         SSLEngine e = c.clientContext.createSSLEngine();
