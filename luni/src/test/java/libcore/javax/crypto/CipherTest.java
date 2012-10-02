2f3704e69938b1cecbd6bc97f1247ec5f3ad03d2
Kenny Root <kroot@google.com>
2012-10-02 09:27:54 -0700

    Test key wrapping for all Ciphers
    Change-Id: I1320f30602e17b730feae5676e34b1550f8eb8b8

--- luni/src/test/java/libcore/javax/crypto/CipherTest.java
+++ luni/src/test/java/libcore/javax/crypto/CipherTest.java
@@ -85,20 +85,6 @@ public final class CipherTest extends TestCase {
         return false;
     }
 
-    private synchronized static int getEncryptMode(String algorithm) throws Exception {
-        if (isWrap(algorithm)) {
-            return Cipher.WRAP_MODE;
-        }
-        return Cipher.ENCRYPT_MODE;
-    }
-
-    private synchronized static int getDecryptMode(String algorithm) throws Exception {
-        if (isWrap(algorithm)) {
-            return Cipher.UNWRAP_MODE;
-        }
-        return Cipher.DECRYPT_MODE;
-    }
-
     private static String getBaseAlgorithm(String algorithm) {
         if (algorithm.equals("AESWRAP")) {
             return "AES";
@@ -173,7 +159,7 @@ public final class CipherTest extends TestCase {
         return getBaseAlgorithm(algorithm).equals("RSA");
     }
 
-    private static boolean isWrap(String algorithm) {
+    private static boolean isOnlyWrappingAlgorithm(String algorithm) {
         return algorithm.endsWith("WRAP");
     }
 
@@ -562,7 +548,12 @@ public final class CipherTest extends TestCase {
         } else {
             spec = null;
         }
-        c.init(getEncryptMode(algorithm), encryptKey, spec);
+
+        if (!isOnlyWrappingAlgorithm(algorithm)) {
+            c.init(Cipher.ENCRYPT_MODE, encryptKey, spec);
+        } else {
+            c.init(Cipher.WRAP_MODE, encryptKey, spec);
+        }
 
         assertEquals("getBlockSize()", getExpectedBlockSize(algorithm, encryptKey),
                 c.getBlockSize());
@@ -576,22 +567,35 @@ public final class CipherTest extends TestCase {
 
         assertNull(c.getExemptionMechanism());
 
-        if (isWrap(algorithm)) {
-            byte[] cipherText = c.wrap(encryptKey);
-            c.init(getDecryptMode(algorithm), getDecryptKey(algorithm));
-            int keyType = (isAsymmetric(algorithm)) ? Cipher.PRIVATE_KEY : Cipher.SECRET_KEY;
-            Key decryptedKey = c.unwrap(cipherText, encryptKey.getAlgorithm(), keyType);
-            assertEquals("encryptKey.getAlgorithm()=" + encryptKey.getAlgorithm()
-                         + " decryptedKey.getAlgorithm()=" + decryptedKey.getAlgorithm()
-                         + " encryptKey.getEncoded()=" + Arrays.toString(encryptKey.getEncoded())
-                         + " decryptedKey.getEncoded()=" + Arrays.toString(decryptedKey.getEncoded()),
-                         encryptKey, decryptedKey);
-        } else {
+        // Test wrapping a key. Every cipher should be able to wrap.
+        {
+            // Generate a small SecretKey for AES.
+            KeyGenerator kg = KeyGenerator.getInstance("AES");
+            kg.init(128);
+            SecretKey sk = kg.generateKey();
+
+            // Wrap it
+            c.init(Cipher.WRAP_MODE, encryptKey, spec);
+            byte[] cipherText = c.wrap(sk);
+
+            // Unwrap it
+            c.init(Cipher.UNWRAP_MODE, getDecryptKey(algorithm), spec);
+            Key decryptedKey = c.unwrap(cipherText, sk.getAlgorithm(), Cipher.SECRET_KEY);
+
+            assertEquals("sk.getAlgorithm()=" + sk.getAlgorithm()
+                    + " decryptedKey.getAlgorithm()=" + decryptedKey.getAlgorithm()
+                    + " encryptKey.getEncoded()=" + Arrays.toString(sk.getEncoded())
+                    + " decryptedKey.getEncoded()=" + Arrays.toString(decryptedKey.getEncoded()),
+                    sk, decryptedKey);
+        }
+
+        if (!isOnlyWrappingAlgorithm(algorithm)) {
+            c.init(Cipher.ENCRYPT_MODE, encryptKey, spec);
             byte[] cipherText = c.doFinal(ORIGINAL_PLAIN_TEXT);
-            c.init(getDecryptMode(algorithm), getDecryptKey(algorithm), spec);
+            c.init(Cipher.DECRYPT_MODE, getDecryptKey(algorithm), spec);
             byte[] decryptedPlainText = c.doFinal(cipherText);
             assertEquals(Arrays.toString(getExpectedPlainText(algorithm)),
-                         Arrays.toString(decryptedPlainText));
+                    Arrays.toString(decryptedPlainText));
         }
     }
 
