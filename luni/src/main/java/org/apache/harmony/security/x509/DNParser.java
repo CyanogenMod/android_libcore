5692d40be0e19ef1d0cc988a579576496adf5237
Brian Carlstrom <bdc@google.com>
2012-10-11 11:05:25 -0700

    Fix typos from "beed" to "been"
    Bug: http://code.google.com/p/android/issues/detail?id=38366
    Change-Id: I44c647843e66a57bcbeabad41ec3142faaa1848e

--- luni/src/main/java/org/apache/harmony/security/x509/DNParser.java
+++ luni/src/main/java/org/apache/harmony/security/x509/DNParser.java
@@ -227,7 +227,7 @@ public final class DNParser {
             case '+':
             case ',':
             case ';':
-                // separator char has beed found
+                // separator char has been found
                 return new String(chars, beg, end - beg);
             case '\\':
                 // escaped char
@@ -247,7 +247,7 @@ public final class DNParser {
                 }
                 if (pos == chars.length || chars[pos] == ',' || chars[pos] == '+'
                         || chars[pos] == ';') {
-                    // separator char or the end of DN has beed found
+                    // separator char or the end of DN has been found
                     return new String(chars, beg, cur - beg);
                 }
                 break;
