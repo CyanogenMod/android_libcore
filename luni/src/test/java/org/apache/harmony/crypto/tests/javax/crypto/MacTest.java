a45d02e5fbf1ec387dcb1e6c91e867d32ab36193
Kenny Root <kroot@google.com>
2013-01-22 16:01:08 -0800

    NativeCrypto: add OpenSSLSecretKey for HMAC
    Change-Id: Id5a77b41549944d7deffda0e2a4e60dbbd26184e

--- luni/src/test/java/org/apache/harmony/crypto/tests/javax/crypto/MacTest.java
+++ luni/src/test/java/org/apache/harmony/crypto/tests/javax/crypto/MacTest.java
@@ -44,6 +44,9 @@ import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.harmony.crypto.tests.support.MyMacSpi;
 import org.apache.harmony.security.tests.support.SpiEngUtils;
+import org.apache.harmony.xnet.provider.jsse.NativeCryptoTest;
+import org.apache.harmony.xnet.provider.jsse.OpenSSLEngine;
+import org.apache.harmony.xnet.provider.jsse.OpenSSLProvider;
 
 import junit.framework.TestCase;
 
@@ -886,6 +889,46 @@ public class MacTest extends TestCase {
             }
 
         }
+    }
+
+    public void test_getInstance_OpenSSL_ENGINE() throws Exception {
+        final String secret = "-HMAC-test1";
+        final byte[] testString = "testing123".getBytes();
+
+        Provider p = Security.getProvider(OpenSSLProvider.PROVIDER_NAME);
+        NativeCryptoTest.loadTestEngine();
+        OpenSSLEngine engine = OpenSSLEngine.getInstance("test");
+
+        /*
+         * The "-HMAC-" prefix is a special prefix recognized by
+         * test_openssl_engine.cpp
+         */
+        SecretKey key1 = engine.getSecretKeyById(secret, "HmacSHA256");
+        SecretKey key1dupe = engine.getSecretKeyById(secret, "HmacSHA256");
+
+        /* Non-ENGINE-based SecretKey */
+        SecretKey key2 = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+
+        /* The one that is ENGINE-based can't be equal to a non-ENGINE one. */
+        assertFalse(key1.equals(key2));
+        assertEquals(key1, key1dupe);
+        assertNull(key1.getFormat());
+        assertNull(key1.getEncoded());
+        assertEquals("RAW", key2.getFormat());
+        assertEquals(Arrays.toString(secret.getBytes()), Arrays.toString(key2.getEncoded()));
+
+        Mac mac1 = Mac.getInstance("HmacSHA256", p);
+        mac1.init(key1);
+        mac1.update(testString);
+        byte[] output1 = mac1.doFinal();
+        assertEquals(mac1.getMacLength(), output1.length);
+
+        Mac mac2 = Mac.getInstance("HmacSHA256", p);
+        mac2.init(key2);
+        mac2.update(testString);
+        byte[] output2 = mac2.doFinal();
+
+        assertEquals(Arrays.toString(output2), Arrays.toString(output1));
     }
 
     class Mock_Mac extends Mac {
