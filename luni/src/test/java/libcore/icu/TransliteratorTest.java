f273ce458c68f5f2b468927392aededc52b8d814
Jay Shrauner <shrauner@google.com>
2013-02-16 08:53:32 -0800

    Persist Transliterator objects with JNI wrappers
    Hold a reference to created Transliterator objects.
    
    Bug:
    Change-Id: I357a4a6670359ff44986bca338725232dc893dc5

--- luni/src/test/java/libcore/icu/TransliteratorTest.java
+++ luni/src/test/java/libcore/icu/TransliteratorTest.java
@@ -20,13 +20,14 @@ public class TransliteratorTest extends junit.framework.TestCase {
   public void testAll() throws Exception {
     for (String id : Transliterator.getAvailableIDs()) {
       System.err.println(id);
-      Transliterator.transliterate(id, "hello");
+      Transliterator t = new Transliterator(id);
+      t.transliterate("hello");
     }
   }
 
   public void test_Unknown() throws Exception {
     try {
-      Transliterator.transliterate("Unknown", "hello");
+      Transliterator t = new Transliterator("Unknown");
       fail();
     } catch (RuntimeException expected) {
     }
@@ -34,7 +35,7 @@ public class TransliteratorTest extends junit.framework.TestCase {
 
   public void test_null_id() throws Exception {
     try {
-      Transliterator.transliterate(null, "hello");
+      Transliterator t = new Transliterator(null);
       fail();
     } catch (NullPointerException expected) {
     }
@@ -42,44 +43,58 @@ public class TransliteratorTest extends junit.framework.TestCase {
 
   public void test_null_string() throws Exception {
     try {
-      Transliterator.transliterate("Any-Upper", null);
+      Transliterator t = new Transliterator("Any-Upper");
+      t.transliterate(null);
       fail();
     } catch (NullPointerException expected) {
     }
   }
 
   public void test_Any_Upper() throws Exception {
-    assertEquals("HELLO WORLD!", Transliterator.transliterate("Any-Upper", "HeLlO WoRlD!"));
-    assertEquals("STRASSE", Transliterator.transliterate("Any-Upper", "Straße"));
+    Transliterator t = new Transliterator("Any-Upper");
+    assertEquals("HELLO WORLD!", t.transliterate("HeLlO WoRlD!"));
+    assertEquals("STRASSE", t.transliterate("Straße"));
   }
 
   public void test_Any_Lower() throws Exception {
-    assertEquals("hello world!", Transliterator.transliterate("Any-Lower", "HeLlO WoRlD!"));
+    Transliterator t = new Transliterator("Any-Lower");
+    assertEquals("hello world!", t.transliterate("HeLlO WoRlD!"));
   }
 
   public void test_Greek_Latin() throws Exception {
     String greek = "Καλημέρα κόσμε!";
 
     // Transliterate Greek to Latin, then to plain ASCII.
-    String latin = Transliterator.transliterate("Greek-Latin", greek);
-    String ascii = Transliterator.transliterate("Latin-Ascii", latin);
+    Transliterator t = new Transliterator("Greek-Latin");
+    String latin = t.transliterate(greek);
+    t = new Transliterator("Latin-Ascii");
+    String ascii = t.transliterate(latin);
     assertEquals("Kalēméra kósme!", latin);
     assertEquals("Kalemera kosme!", ascii);
 
     // Use alternative transliteration variants.
-    assertEquals("Kaliméra kósme!", Transliterator.transliterate("Greek-Latin/BGN", greek));
-    assertEquals("Kali̱méra kósme!", Transliterator.transliterate("Greek-Latin/UNGEGN", greek));
+    t = new Transliterator("Greek-Latin/BGN");
+    assertEquals("Kaliméra kósme!", t.transliterate(greek));
+    t = new Transliterator("Greek-Latin/UNGEGN");
+    assertEquals("Kali̱méra kósme!",t.transliterate(greek));
   }
 
   public void test_Han_Latin() throws Exception {
-    assertEquals("hàn zì/hàn zì", Transliterator.transliterate("Han-Latin", "汉字/漢字"));
+    Transliterator t = new Transliterator("Han-Latin");
+    assertEquals("hàn zì/hàn zì", t.transliterate("汉字/漢字"));
 
-    assertEquals("chén", Transliterator.transliterate("Han-Latin", "\u6c88"));
-    assertEquals("shěn", Transliterator.transliterate("Han-Latin", "\u700b"));
-    assertEquals("jiǎ", Transliterator.transliterate("Han-Latin", "\u8d3e"));
+    assertEquals("chén", t.transliterate("\u6c88"));
+    assertEquals("shěn", t.transliterate("\u700b"));
+    assertEquals("jiǎ", t.transliterate("\u8d3e"));
 
-    assertEquals("shěn", Transliterator.transliterate("Han-Latin/Names", "\u6c88"));
-    assertEquals("shěn", Transliterator.transliterate("Han-Latin/Names", "\u700b"));
-    assertEquals("jǐa", Transliterator.transliterate("Han-Latin/Names", "\u8d3e"));
+    t = new Transliterator("Han-Latin/Names");
+    assertEquals("shěn", t.transliterate("\u6c88"));
+    assertEquals("shěn", t.transliterate("\u700b"));
+    assertEquals("jǐa", t.transliterate("\u8d3e"));
+
+    t = new Transliterator("Han-Latin/Names; Latin-Ascii; Any-Upper");
+    assertEquals("SHEN", t.transliterate("\u6c88"));
+    assertEquals("SHEN", t.transliterate("\u700b"));
+    assertEquals("JIA", t.transliterate("\u8d3e"));
   }
 }
