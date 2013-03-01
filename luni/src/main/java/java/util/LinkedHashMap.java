e2f220961759e31c94bfa770b6a6ddbf019c16d9
Elliott Hughes <enh@google.com>
2013-02-28 16:12:25 -0800

    Fix the HashMap performance regression.
    Go back to the original cheap (and inline) secondary hash.
    LinkedHashMap is a subclass of HashMap, so that has to change too.
    
    Bug: 8290590
    Change-Id: I7959a8ce8b8fc96e7f1d2c8a2a13fe83ef4c0dd5

--- luni/src/main/java/java/util/LinkedHashMap.java
+++ luni/src/main/java/java/util/LinkedHashMap.java
@@ -247,7 +247,8 @@ public class LinkedHashMap<K, V> extends HashMap<K, V> {
             return e.value;
         }
 
-        int hash = Collections.secondaryHash(key);
+        // Replace with Collections.secondaryHash when the VM is fast enough (http://b/8290590).
+        int hash = secondaryHash(key);
         HashMapEntry<K, V>[] tab = table;
         for (HashMapEntry<K, V> e = tab[hash & (tab.length - 1)];
                 e != null; e = e.next) {
