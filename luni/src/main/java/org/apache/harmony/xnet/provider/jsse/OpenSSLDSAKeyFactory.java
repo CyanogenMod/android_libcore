multi-os-engine/libcore

Disallow KeyFactory.translateKey between public and private
This fixes the bug where OpenSSL-backed KeyFactory instances (RSA,
DSA, EC) could translateKey from PrivateKey to PublicKey and vice
versa when presented with "opaque" keys whose translation is
performed via their primary encoded form.

Change-Id: Ic2e24343ff7312482d0e90e390a7c4b6b21d07da

luni/src/main/java/org/apache/harmony/xnet/provider/jsse/OpenSSLDSAKeyFactory.java
@@ -172,13 +172,13 @@ public class OpenSSLDSAKeyFactory extends KeyFactorySpi {
             } catch (InvalidKeySpecException e) {
                 throw new InvalidKeyException(e);
             }
-        } else if ("PKCS#8".equals(key.getFormat())) {
+        } else if ((key instanceof PrivateKey) && ("PKCS#8".equals(key.getFormat()))) {
             try {
                 return engineGeneratePrivate(new PKCS8EncodedKeySpec(key.getEncoded()));
             } catch (InvalidKeySpecException e) {
                 throw new InvalidKeyException(e);
             }
-        } else if ("X.509".equals(key.getFormat())) {
+        } else if ((key instanceof PrivateKey) && ("X.509".equals(key.getFormat()))) {
             try {
                 return engineGeneratePublic(new X509EncodedKeySpec(key.getEncoded()));
             } catch (InvalidKeySpecException e) {

luni/src/main/java/org/apache/harmony/xnet/provider/jsse/OpenSSLRSAKeyFactory.java
@@ -202,13 +202,13 @@ public class OpenSSLRSAKeyFactory extends KeyFactorySpi {
             } catch (InvalidKeySpecException e) {
                 throw new InvalidKeyException(e);
             }
-        } else if ("PKCS#8".equals(key.getFormat())) {
+        } else if ((key instanceof PrivateKey) && ("PKCS#8".equals(key.getFormat()))) {
             try {
                 return engineGeneratePrivate(new PKCS8EncodedKeySpec(key.getEncoded()));
             } catch (InvalidKeySpecException e) {
                 throw new InvalidKeyException(e);
             }
-        } else if ("X.509".equals(key.getFormat())) {
+        } else if ((key instanceof PrivateKey) && ("X.509".equals(key.getFormat()))) {
             try {
                 return engineGeneratePublic(new X509EncodedKeySpec(key.getEncoded()));
             } catch (InvalidKeySpecException e) {

CyanogenMod/android_libcore

Change-Id: Ia24a2591a06ac82c5225a3a9e3069af7a01a0c37

luni/src/main/java/org/apache/harmony/xnet/provider/jsse/OpenSSLDSAKeyFactory.java
@@ -172,7 +172,7 @@ public class OpenSSLDSAKeyFactory extends KeyFactorySpi {
             } catch (InvalidKeySpecException e) {
                 throw new InvalidKeyException(e);
             }
-        } else if ("PKCS#8".equals(key.getFormat())) {
+        } else if ((key instanceof PrivateKey) && ("PKCS#8".equals(key.getFormat()))) {
             byte[] encoded = key.getEncoded();
             if (encoded == null) {
                 throw new InvalidKeyException("Key does not support encoding");
@@ -182,7 +182,7 @@ public class OpenSSLDSAKeyFactory extends KeyFactorySpi {
             } catch (InvalidKeySpecException e) {
                 throw new InvalidKeyException(e);
             }
-        } else if ("X.509".equals(key.getFormat())) {
+        } else if ((key instanceof PublicKey) && ("X.509".equals(key.getFormat()))) {
             byte[] encoded = key.getEncoded();
             if (encoded == null) {
                 throw new InvalidKeyException("Key does not support encoding");

luni/src/main/java/org/apache/harmony/xnet/provider/jsse/OpenSSLRSAKeyFactory.java
@@ -202,7 +202,7 @@ public class OpenSSLRSAKeyFactory extends KeyFactorySpi {
             } catch (InvalidKeySpecException e) {
                 throw new InvalidKeyException(e);
             }
-        } else if ("PKCS#8".equals(key.getFormat())) {
+        } else if ((key instanceof PrivateKey) && ("PKCS#8".equals(key.getFormat()))) {
             byte[] encoded = key.getEncoded();
             if (encoded == null) {
                 throw new InvalidKeyException("Key does not support encoding");
@@ -212,7 +212,7 @@ public class OpenSSLRSAKeyFactory extends KeyFactorySpi {
             } catch (InvalidKeySpecException e) {
                 throw new InvalidKeyException(e);
             }
-        } else if ("X.509".equals(key.getFormat())) {
+        } else if ((key instanceof PublicKey) && ("X.509".equals(key.getFormat()))) {
             byte[] encoded = key.getEncoded();
             if (encoded == null) {
                 throw new InvalidKeyException("Key does not support encoding");