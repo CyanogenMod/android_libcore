adf1aa88864d176ab08f95b9ea6fffc744b64ae7
Elliott Hughes <enh@google.com>
2013-10-15 13:17:37 -0700

    Improve the charAt documentation.
    Bug: https://code.google.com/p/android/issues/detail?id=61010
    Change-Id: Iecdda7dfd9d47b2269ae7713b4a7cdb340f9219a

--- luni/src/main/java/java/lang/AbstractStringBuilder.java
+++ luni/src/main/java/java/lang/AbstractStringBuilder.java
@@ -192,14 +192,8 @@ abstract class AbstractStringBuilder {
     }
 
     /**
-     * Retrieves the character at the {@code index}.
-     *
-     * @param index
-     *            the index of the character to retrieve.
-     * @return the char value.
-     * @throws IndexOutOfBoundsException
-     *             if {@code index} is negative or greater than or equal to the
-     *             current {@link #length()}.
+     * Returns the character at {@code index}.
+     * @throws IndexOutOfBoundsException if {@code index < 0} or {@code index >= length()}.
      */
     public char charAt(int index) {
         if (index < 0 || index >= count) {
