59351c4e69b0e917d1f60e7ae32e76615150690c
Jeff Hao <jeffhao@google.com>
2013-10-28 18:26:32 -0700

    Libcore support for Interface Method Tables (IMT).
    (cherry picked from commit 500f98f9696baad7ba16bd5ad65e8caa475ebfe7)
    
    Change-Id: I0d611ba7b24f2a990a4a2958939b0f6d739d3c14

--- libart/src/main/java/java/lang/Class.java
+++ libart/src/main/java/java/lang/Class.java
@@ -166,6 +166,9 @@ public final class Class<T> implements Serializable, AnnotatedElement, GenericDe
      */
     private transient Object[] ifTable;
 
+    /** Interface method table (imt), for quick "invoke-interface". */
+    private transient ArtMethod[] imTable;
+
     /** Lazily computed name of this class; always prefer calling getName(). */
     private transient String name;
 
