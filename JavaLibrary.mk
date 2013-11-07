d59063c7a6eb985a857ecbf2ca8d1b6dc5deac1d
Narayan Kamath <narayan@google.com>
2013-11-07 17:39:27 +0000

    Remove incorrect LOCAL_NO_STANDARD_LIBRARIES flag.
    (cherry-picked from commit 115197d88b5ce8a4a25a6deaa172cd1848e4f9d6)
    
    Change-Id: Ie0f2d0b5fb88e6a507836db3609b4be53abb636b

--- JavaLibrary.mk
+++ JavaLibrary.mk
@@ -196,7 +196,6 @@ ifeq ($(WITH_HOST_DALVIK),true)
     include $(CLEAR_VARS)
     LOCAL_SRC_FILES := $(test_src_files)
     LOCAL_JAVA_RESOURCE_DIRS := $(test_resource_dirs)
-    LOCAL_NO_STANDARD_LIBRARIES := true
     LOCAL_JAVA_LIBRARIES := bouncycastle-hostdex core-junit-hostdex core-tests-support-hostdex okhttp-hostdex
     LOCAL_STATIC_JAVA_LIBRARIES := sqlite-jdbc-host mockwebserver-host nist-pkix-tests-host
     LOCAL_JAVACFLAGS := $(local_javac_flags)
