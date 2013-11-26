multi-os-engine/libcore

Assert that getEnabledCipherSuites/Protocols is affected by setters.
Change-Id: I806cfd4fd7f8be39c9bfcce956f6c2804e917c26

luni/src/test/java/libcore/javax/net/ssl/SSLEngineTest.java
@@ -211,6 +211,12 @@ public class SSLEngineTest extends TestCase {
         e.setEnabledCipherSuites(new String[0]);
         e.setEnabledCipherSuites(e.getEnabledCipherSuites());
         e.setEnabledCipherSuites(e.getSupportedCipherSuites());
+
+        // Check that setEnabledCipherSuites affects getEnabledCipherSuites
+        String[] cipherSuites = new String[] {e.getSupportedCipherSuites()[0]};
+        e.setEnabledCipherSuites(cipherSuites);
+        assertEquals(Arrays.asList(cipherSuites), Arrays.asList(e.getEnabledCipherSuites()));
+
         c.close();
     }
 
@@ -250,6 +256,12 @@ public class SSLEngineTest extends TestCase {
         e.setEnabledProtocols(new String[0]);
         e.setEnabledProtocols(e.getEnabledProtocols());
         e.setEnabledProtocols(e.getSupportedProtocols());
+
+        // Check that setEnabledProtocols affects getEnabledProtocols
+        String[] protocols = new String[] {e.getSupportedProtocols()[0]};
+        e.setEnabledProtocols(protocols);
+        assertEquals(Arrays.asList(protocols), Arrays.asList(e.getEnabledProtocols()));
+
         c.close();
     }
 

luni/src/test/java/libcore/javax/net/ssl/SSLSocketTest.java
@@ -254,6 +254,11 @@ public class SSLSocketTest extends TestCase {
         ssl.setEnabledCipherSuites(new String[0]);
         ssl.setEnabledCipherSuites(ssl.getEnabledCipherSuites());
         ssl.setEnabledCipherSuites(ssl.getSupportedCipherSuites());
+
+        // Check that setEnabledCipherSuites affects getEnabledCipherSuites
+        String[] cipherSuites = new String[] {ssl.getSupportedCipherSuites()[0]};
+        ssl.setEnabledCipherSuites(cipherSuites);
+        assertEquals(Arrays.asList(cipherSuites), Arrays.asList(ssl.getEnabledCipherSuites()));
     }
 
     public void test_SSLSocket_getSupportedProtocols_returnsCopies() throws Exception {
@@ -290,6 +295,11 @@ public class SSLSocketTest extends TestCase {
         ssl.setEnabledProtocols(new String[0]);
         ssl.setEnabledProtocols(ssl.getEnabledProtocols());
         ssl.setEnabledProtocols(ssl.getSupportedProtocols());
+
+        // Check that setEnabledProtocols affects getEnabledProtocols
+        String[] protocols = new String[] {ssl.getSupportedProtocols()[0]};
+        ssl.setEnabledProtocols(protocols);
+        assertEquals(Arrays.asList(protocols), Arrays.asList(ssl.getEnabledProtocols()));
     }
 
     public void test_SSLSocket_getSession() throws Exception {

CyanogenMod/android_libcore

luni/src/test/java/libcore/javax/net/ssl/SSLEngineTest.java
@@ -250,6 +250,12 @@ public class SSLEngineTest extends TestCase {
         e.setEnabledCipherSuites(new String[0]);
         e.setEnabledCipherSuites(e.getEnabledCipherSuites());
         e.setEnabledCipherSuites(e.getSupportedCipherSuites());
+
+        // Check that setEnabledCipherSuites affects getEnabledCipherSuites
+        String[] cipherSuites = new String[] { e.getSupportedCipherSuites()[0] };
+        e.setEnabledCipherSuites(cipherSuites);
+        assertEquals(Arrays.asList(cipherSuites), Arrays.asList(e.getEnabledCipherSuites()));
+
         c.close();
     }
 
@@ -289,6 +295,12 @@ public class SSLEngineTest extends TestCase {
         e.setEnabledProtocols(new String[0]);
         e.setEnabledProtocols(e.getEnabledProtocols());
         e.setEnabledProtocols(e.getSupportedProtocols());
+
+        // Check that setEnabledProtocols affects getEnabledProtocols
+        String[] protocols = new String[] { e.getSupportedProtocols()[0] };
+        e.setEnabledProtocols(protocols);
+        assertEquals(Arrays.asList(protocols), Arrays.asList(e.getEnabledProtocols()));
+
         c.close();
     }
 

luni/src/test/java/libcore/javax/net/ssl/SSLSocketTest.java
@@ -254,6 +254,11 @@ public class SSLSocketTest extends TestCase {
         ssl.setEnabledCipherSuites(new String[0]);
         ssl.setEnabledCipherSuites(ssl.getEnabledCipherSuites());
         ssl.setEnabledCipherSuites(ssl.getSupportedCipherSuites());
+
+        // Check that setEnabledCipherSuites affects getEnabledCipherSuites
+        String[] cipherSuites = new String[] { ssl.getSupportedCipherSuites()[0] };
+        ssl.setEnabledCipherSuites(cipherSuites);
+        assertEquals(Arrays.asList(cipherSuites), Arrays.asList(ssl.getEnabledCipherSuites()));
     }
 
     public void test_SSLSocket_getSupportedProtocols_returnsCopies() throws Exception {
@@ -290,6 +295,11 @@ public class SSLSocketTest extends TestCase {
         ssl.setEnabledProtocols(new String[0]);
         ssl.setEnabledProtocols(ssl.getEnabledProtocols());
         ssl.setEnabledProtocols(ssl.getSupportedProtocols());
+
+        // Check that setEnabledProtocols affects getEnabledProtocols
+        String[] protocols = new String[] { ssl.getSupportedProtocols()[0] };
+        ssl.setEnabledProtocols(protocols);
+        assertEquals(Arrays.asList(protocols), Arrays.asList(ssl.getEnabledProtocols()));
     }
 
     public void test_SSLSocket_getSession() throws Exception {