3cc0bda1e2573caf5cfafaaef08eb6b5beba5c0a
Narayan Kamath <narayan@google.com>
2014-01-22 12:53:31 +0000

    Fix ComputeSerialVersionUIDTest.
    The old golden files were referring to the wrong class,
    and contained serial version UIDs that didn't match those
    generated by any version of android (or the RI).
    
    This change deletes the golden files and switches to
    direct assertions in the test. All tests pass on
    aosp-master, v4.xx of android and the RI.
    
    bug: 12490445
    
    Change-Id: Ia65b6d65bc9a3d4cbc62a7a0f7bdfc004e7315e2

--- harmony-tests/src/test/java/org/apache/harmony/tests/java/io/ComputeSerialVersionUIDTest.java
+++ harmony-tests/src/test/java/org/apache/harmony/tests/java/io/ComputeSerialVersionUIDTest.java
@@ -1,145 +1,86 @@
 package org.apache.harmony.tests.java.io;
 
-import java.io.ObjectInputStream;
+import java.io.ObjectStreamClass;
+
 
 public class ComputeSerialVersionUIDTest extends junit.framework.TestCase {
 
-
-    private String path = "/serialization/org/apache/harmony/tests/java/io/";
-
     public void testComputeSUIDClass() throws Exception {
-        ObjectInputStream ois = new ObjectInputStream(getClass()
-                .getResourceAsStream(path + "testComputeSUIDClass.ser"));
-        SerializationTestClass.TestClassName1 o1 = (SerializationTestClass.TestClassName1) ois
-                .readObject();
-        SerializationTestClass.TestClassName2T_T$T o2 = (SerializationTestClass.TestClassName2T_T$T) ois
-                .readObject();
-        SerializationTestClass.TestClassModifierPublic o6 = (SerializationTestClass.TestClassModifierPublic) ois
-                .readObject();
-        SerializationTestClass.TestClassModifierAbstract o3 = (SerializationTestClass.TestClassModifierAbstract) ois
-                .readObject();
-        SerializationTestClass.TestClassModifierFinal o4 = (SerializationTestClass.TestClassModifierFinal) ois
-                .readObject();
-        SerializationTestClass.TestClassModifierInterface o5 = (SerializationTestClass.TestClassModifierInterface) ois
-                .readObject();
-        ois.close();
+        assertEquals(-5877374382732244721L,
+                computeSerialVersionUID(SerializationTestClass.TestClassName1.class));
+        assertEquals(-2258784348609133821L,
+                computeSerialVersionUID(SerializationTestClass.TestClassName2T_T$T.class));
+        assertEquals(-5674447587118957354L,
+                computeSerialVersionUID(SerializationTestClass.TestClassModifierPublic.class));
+        assertEquals(8333249076871004334L,
+                computeSerialVersionUID(SerializationTestClass.TestClassModifierAbstract.class));
+        assertEquals(-6752991881983868187L,
+                computeSerialVersionUID(SerializationTestClass.TestClassModifierFinal.class));
+        assertEquals(-2046603329186110997L,
+                computeSerialVersionUID(SerializationTestClass.TestClassModifierInterface.class));
     }
 
     public void testComputeSUIDInterfaces() throws Exception {
-        ObjectInputStream ois = new ObjectInputStream(getClass()
-                .getResourceAsStream(path + "testComputeSUIDInterfaces.ser"));
-        SerializationTestClass.TestIntefaces o1 = (SerializationTestClass.TestIntefaces) ois
-                .readObject();
-        SerializationTestClass.TestIntefacesA o2 = (SerializationTestClass.TestIntefacesA) ois
-                .readObject();
-        SerializationTestClass.TestIntefacesAB o3 = (SerializationTestClass.TestIntefacesAB) ois
-                .readObject();
-        SerializationTestClass.TestIntefacesBA o4 = (SerializationTestClass.TestIntefacesBA) ois
-                .readObject();
-        SerializationTestClass.TestIntefacesC o5 = (SerializationTestClass.TestIntefacesC) ois
-                .readObject();
-        SerializationTestClass.TestIntefacesAC o6 = (SerializationTestClass.TestIntefacesAC) ois
-                .readObject();
-        SerializationTestClass.TestIntefacesCA o7 = (SerializationTestClass.TestIntefacesCA) ois
-                .readObject();
-        SerializationTestClass.TestIntefacesABC o8 = (SerializationTestClass.TestIntefacesABC) ois
-                .readObject();
-        SerializationTestClass.TestIntefacesACB o9 = (SerializationTestClass.TestIntefacesACB) ois
-                .readObject();
-        SerializationTestClass.TestIntefacesBAC o10 = (SerializationTestClass.TestIntefacesBAC) ois
-                .readObject();
-        SerializationTestClass.TestIntefacesBCA o11 = (SerializationTestClass.TestIntefacesBCA) ois
-                .readObject();
-        SerializationTestClass.TestIntefacesCAB o12 = (SerializationTestClass.TestIntefacesCAB) ois
-                .readObject();
-        SerializationTestClass.TestIntefacesCBA o13 = (SerializationTestClass.TestIntefacesCBA) ois
-                .readObject();
-        ois.close();
+        assertEquals(2385879270919801624L, computeSerialVersionUID(SerializationTestClass.TestInterfaces.class));
+        assertEquals(-3876044724689092051L, computeSerialVersionUID(SerializationTestClass.TestInterfacesA.class));
+        assertEquals(6691168002125833763L, computeSerialVersionUID(SerializationTestClass.TestInterfacesAB.class));
+        assertEquals(-3862602835688739317L, computeSerialVersionUID(SerializationTestClass.TestInterfacesBA.class));
+        assertEquals(6153219913626150137L, computeSerialVersionUID(SerializationTestClass.TestInterfacesC.class));
+        assertEquals(-5230940296111061949L, computeSerialVersionUID(SerializationTestClass.TestInterfacesCA.class));
+        assertEquals(-561891731488612449L, computeSerialVersionUID(SerializationTestClass.TestInterfacesABC.class));
+        assertEquals(7173098887933679885L, computeSerialVersionUID(SerializationTestClass.TestInterfacesACB.class));
+        assertEquals(7417451177210251082L, computeSerialVersionUID(SerializationTestClass.TestInterfacesBAC.class));
+        assertEquals(6457265192863049241L, computeSerialVersionUID(SerializationTestClass.TestInterfacesBCA.class));
+        assertEquals( 5890948387530452778L, computeSerialVersionUID(SerializationTestClass.TestInterfacesCAB.class));
+        assertEquals(-7493935950381842313L, computeSerialVersionUID(SerializationTestClass.TestInterfacesCBA.class));
     }
+
 
     public void testComputeSUIDFields() throws Exception {
-        ObjectInputStream ois = new ObjectInputStream(getClass()
-                .getResourceAsStream(path + "testComputeSUIDFields.ser"));
-        SerializationTestClass.TestFieldsNone o1 = (SerializationTestClass.TestFieldsNone) ois
-                .readObject();
-        SerializationTestClass.TestFieldsOneFinal o2 = (SerializationTestClass.TestFieldsOneFinal) ois
-                .readObject();
-        SerializationTestClass.TestFieldsTwoFinal o3 = (SerializationTestClass.TestFieldsTwoFinal) ois
-                .readObject();
-        SerializationTestClass.TestFieldsOnePrivate o4 = (SerializationTestClass.TestFieldsOnePrivate) ois
-                .readObject();
-        SerializationTestClass.TestFieldsTwoPrivate o5 = (SerializationTestClass.TestFieldsTwoPrivate) ois
-                .readObject();
-        SerializationTestClass.TestFieldsOneProtected o6 = (SerializationTestClass.TestFieldsOneProtected) ois
-                .readObject();
-        SerializationTestClass.TestFieldsTwoProtected o7 = (SerializationTestClass.TestFieldsTwoProtected) ois
-                .readObject();
-        SerializationTestClass.TestFieldsOnePublic o8 = (SerializationTestClass.TestFieldsOnePublic) ois
-                .readObject();
-        SerializationTestClass.TestFieldsTwoPublic o9 = (SerializationTestClass.TestFieldsTwoPublic) ois
-                .readObject();
-        SerializationTestClass.TestFieldsOneStatic o10 = (SerializationTestClass.TestFieldsOneStatic) ois
-                .readObject();
-        SerializationTestClass.TestFieldsTwoStatic o11 = (SerializationTestClass.TestFieldsTwoStatic) ois
-                .readObject();
-        SerializationTestClass.TestFieldsOneTransient o12 = (SerializationTestClass.TestFieldsOneTransient) ois
-                .readObject();
-        SerializationTestClass.TestFieldsTwoTransient o13 = (SerializationTestClass.TestFieldsTwoTransient) ois
-                .readObject();
-        SerializationTestClass.TestFieldsOneVolatile o14 = (SerializationTestClass.TestFieldsOneVolatile) ois
-                .readObject();
-        SerializationTestClass.TestFieldsTwoVolatile o15 = (SerializationTestClass.TestFieldsTwoVolatile) ois
-                .readObject();
-        SerializationTestClass.TestFieldSignatures o16 = (SerializationTestClass.TestFieldSignatures) ois
-                .readObject();
-        ois.close();
+        assertEquals(-30967666739349603L, computeSerialVersionUID(SerializationTestClass.TestFieldsNone.class));
+        assertEquals(8551211022820107208L, computeSerialVersionUID(SerializationTestClass.TestFieldsOneFinal.class));
+        assertEquals(-7774226929120968860L, computeSerialVersionUID(SerializationTestClass.TestFieldsTwoFinal.class));
+        assertEquals(-8196468848051541845L, computeSerialVersionUID(SerializationTestClass.TestFieldsOnePrivate.class));
+        assertEquals(-7861029019096564216L, computeSerialVersionUID(SerializationTestClass.TestFieldsTwoPrivate.class));
+        assertEquals(81248916710250820L, computeSerialVersionUID(SerializationTestClass.TestFieldsOneProtected.class));
+        assertEquals(280835377416490750L, computeSerialVersionUID(SerializationTestClass.TestFieldsTwoProtected.class));
+        assertEquals(-2290036437752730858L, computeSerialVersionUID(SerializationTestClass.TestFieldsOnePublic.class));
+        assertEquals(-6124932240571007214L, computeSerialVersionUID(SerializationTestClass.TestFieldsTwoPublic.class));
+        assertEquals(6101579853402497691L, computeSerialVersionUID(SerializationTestClass.TestFieldsOneStatic.class));
+        assertEquals(-7900976994549865116L, computeSerialVersionUID(SerializationTestClass.TestFieldsTwoStatic.class));
+        assertEquals(-4499355017417065560L, computeSerialVersionUID(SerializationTestClass.TestFieldsOneTransient.class));
+        assertEquals(3747907454018261619L, computeSerialVersionUID(SerializationTestClass.TestFieldsTwoTransient.class));
+        assertEquals(-4945042592592621725L, computeSerialVersionUID(SerializationTestClass.TestFieldsOneVolatile.class));
+        assertEquals(8983117060325881490L, computeSerialVersionUID(SerializationTestClass.TestFieldsTwoVolatile.class));
+        assertEquals(-8336483965186710722L, computeSerialVersionUID(SerializationTestClass.TestFieldSignatures.class));
     }
 
+
     public void testComputeSUIDConstructors() throws Exception {
-        ObjectInputStream ois = new ObjectInputStream(getClass()
-                .getResourceAsStream(path + "testComputeSUIDConstructors.ser"));
-        SerializationTestClass.TestConstructorNone o1 = (SerializationTestClass.TestConstructorNone) ois
-                .readObject();
-        SerializationTestClass.TestConstructorOne o2 = (SerializationTestClass.TestConstructorOne) ois
-                .readObject();
-        SerializationTestClass.TestConstructorPrivate o3 = (SerializationTestClass.TestConstructorPrivate) ois
-                .readObject();
-        SerializationTestClass.TestConstructorProtected o4 = (SerializationTestClass.TestConstructorProtected) ois
-                .readObject();
-        SerializationTestClass.TestConstructorPublic o5 = (SerializationTestClass.TestConstructorPublic) ois
-                .readObject();
-        SerializationTestClass.TestConstructorSignature o6 = (SerializationTestClass.TestConstructorSignature) ois
-                .readObject();
-        SerializationTestClass.TestConstructorTwo o7 = (SerializationTestClass.TestConstructorTwo) ois
-                .readObject();
-        SerializationTestClass.TestConstructorTwoReverse o8 = (SerializationTestClass.TestConstructorTwoReverse) ois
-                .readObject();
-        ois.close();
+        assertEquals(-614706174292151857L, computeSerialVersionUID(SerializationTestClass.TestConstructorNone.class));
+        assertEquals(-3706135726712902027L, computeSerialVersionUID(SerializationTestClass.TestConstructorOne.class));
+        assertEquals(-8094991171016233719L, computeSerialVersionUID(SerializationTestClass.TestConstructorPrivate.class));
+        assertEquals(-8117933510362198687L, computeSerialVersionUID(SerializationTestClass.TestConstructorProtected.class));
+        assertEquals(9205589590060392077L, computeSerialVersionUID(SerializationTestClass.TestConstructorPublic.class));
+        assertEquals(5408111072458161992L, computeSerialVersionUID(SerializationTestClass.TestConstructorSignature.class));
+        assertEquals(625104530709630511L, computeSerialVersionUID(SerializationTestClass.TestConstructorTwo.class));
+        assertEquals(3737423569701135020L, computeSerialVersionUID(SerializationTestClass.TestConstructorTwoReverse.class));
     }
 
     public void testComputeSUIDMethods() throws Exception {
-        ObjectInputStream ois = new ObjectInputStream(getClass()
-                .getResourceAsStream(path + "testComputeSUIDMethods.ser"));
-        SerializationTestClass.TestMehodPrivate o1 = (SerializationTestClass.TestMehodPrivate) ois
-                .readObject();
-        SerializationTestClass.TestMethodAbstract o2 = (SerializationTestClass.TestMethodAbstract) ois
-                .readObject();
-        SerializationTestClass.TestMethodFinal o3 = (SerializationTestClass.TestMethodFinal) ois
-                .readObject();
-        SerializationTestClass.TestMethodNative o4 = (SerializationTestClass.TestMethodNative) ois
-                .readObject();
-        SerializationTestClass.TestMethodProtected o5 = (SerializationTestClass.TestMethodProtected) ois
-                .readObject();
-        SerializationTestClass.TestMethodPublic o6 = (SerializationTestClass.TestMethodPublic) ois
-                .readObject();
-        SerializationTestClass.TestMethodStatic o7 = (SerializationTestClass.TestMethodStatic) ois
-                .readObject();
-        SerializationTestClass.TestMethodSignature o9 = (SerializationTestClass.TestMethodSignature) ois
-                .readObject();
-        SerializationTestClass.TestMethodReturnSignature o10 = (SerializationTestClass.TestMethodReturnSignature) ois
-                .readObject();
-        SerializationTestClass.TestMethodSynchronized o8 = (SerializationTestClass.TestMethodSynchronized) ois
-                .readObject();
-        ois.close();
+        assertEquals(8872679581767836990L, computeSerialVersionUID(SerializationTestClass.TestMethodPrivate.class));
+        assertEquals(-4558121473827608582L, computeSerialVersionUID(SerializationTestClass.TestMethodAbstract.class));
+        assertEquals(4148772500508720405L, computeSerialVersionUID(SerializationTestClass.TestMethodFinal.class));
+        assertEquals(6329381817306256121L, computeSerialVersionUID(SerializationTestClass.TestMethodNative.class));
+        assertEquals(-2701115429311553102L, computeSerialVersionUID(SerializationTestClass.TestMethodProtected.class));
+        assertEquals(-4092306049997161465L, computeSerialVersionUID(SerializationTestClass.TestMethodPublic.class));
+        assertEquals(-7948580256486289776L, computeSerialVersionUID(SerializationTestClass.TestMethodStatic.class));
+        assertEquals(4085068229405300186L, computeSerialVersionUID(SerializationTestClass.TestMethodSignature.class));
+        assertEquals(-5743322978294773864L, computeSerialVersionUID(SerializationTestClass.TestMethodReturnSignature.class));
+        assertEquals(-6908429504335657476L, computeSerialVersionUID(SerializationTestClass.TestMethodSynchronized.class));
+    }
+
+    private static long computeSerialVersionUID(Class<?> clazz) {
+        return ObjectStreamClass.lookup(clazz).getSerialVersionUID();
     }
 }
