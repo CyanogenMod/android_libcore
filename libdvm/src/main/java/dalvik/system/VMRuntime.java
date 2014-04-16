6174cff204aa36e622e9bd93569b4a1eb2ed222c
Narayan Kamath <narayan@google.com>
2014-04-16 11:19:57 +0100

    Add a mapping between ABIs and instruction sets.
    Bridges the android ABI lists (TARGET_CPU_ABI, TARGET_CPU_ABI2
    from the BoardConfig and android.os.Build.CPU_ABI) and the
    runtime concept of an instruction set used for compilation
    (the --instruction-set to dex2oat and so on).
    
    Change-Id: Ifbc51bf4086c36480a4e636e61494999f5de50ef

--- libdvm/src/main/java/dalvik/system/VMRuntime.java
+++ libdvm/src/main/java/dalvik/system/VMRuntime.java
@@ -16,6 +16,9 @@
 
 package dalvik.system;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Provides an interface to VM-global, Dalvik-specific features.
  * An application cannot create its own Runtime instance, and must obtain
@@ -29,6 +32,15 @@ public final class VMRuntime {
      * Holds the VMRuntime singleton.
      */
     private static final VMRuntime THE_ONE = new VMRuntime();
+
+    private static final Map<String, String> ABI_TO_INSTRUCTION_SET_MAP
+            = new HashMap<String, String>();
+    static {
+        ABI_TO_INSTRUCTION_SET_MAP.put("armeabi", "arm");
+        ABI_TO_INSTRUCTION_SET_MAP.put("armeabi-v7a", "arm");
+        ABI_TO_INSTRUCTION_SET_MAP.put("mips", "mips");
+        ABI_TO_INSTRUCTION_SET_MAP.put("x86", "x86");
+    }
 
     private int targetSdkVersion;
 
@@ -322,5 +334,21 @@ public final class VMRuntime {
      */
     public static void registerAppInfo(String appDir, String processName, String pkgname) {
         // Nothing to do in dalvik.
+    }
+
+    /**
+     * Returns the runtime instruction set corresponding to a given ABI. Multiple
+     * compatible ABIs might map to the same instruction set. For example
+     * {@code armeabi-v7a} and {@code armeabi} might map to the instruction set {@code arm}.
+     *
+     * This influences the compilation of the applications classes.
+     */
+    public static String getInstructionSet(String abi) {
+        final String instructionSet = ABI_TO_INSTRUCTION_SET_MAP.get(abi);
+        if (instructionSet == null) {
+            throw new IllegalArgumentException("Unsupported ABI: " + abi);
+        }
+
+        return instructionSet;
     }
 }
