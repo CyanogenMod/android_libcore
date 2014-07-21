fb6c80875a8a8d0a9628562f89c250b6a962e824
Mathieu Chartier <mathieuc@google.com>
2014-07-21 11:13:29 -0700

    Make ByteBuffer.allocateDirect be 8 byte aligned.
    Bug: 16449607
    
    Change-Id: I4a854d55f49fe709965c6b6a769fb58c356627dc

--- luni/src/main/java/java/nio/ByteBuffer.java
+++ luni/src/main/java/java/nio/ByteBuffer.java
@@ -69,7 +69,11 @@ public abstract class ByteBuffer extends Buffer implements Comparable<ByteBuffer
         if (capacity < 0) {
             throw new IllegalArgumentException("capacity < 0: " + capacity);
         }
-        return new DirectByteBuffer(MemoryBlock.allocate(capacity), capacity, 0, false, null);
+        // Ensure alignment by 8.
+        MemoryBlock memoryBlock = MemoryBlock.allocate(capacity + 7);
+        long address = memoryBlock.toLong();
+        long alignedAddress = (address + 7) & ~(long)7;
+        return new DirectByteBuffer(memoryBlock, capacity, (int)(alignedAddress - address), false, null);
     }
 
     /**
