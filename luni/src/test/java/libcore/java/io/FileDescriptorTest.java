3a1658cb79ccc96e81eca64966de4ada725a2e54
Elliott Hughes <enh@google.com>
2015-01-08 12:30:21 -0800

    Fix and clean up FileDescriptor.isSocket.
    The original logic was accidentally inverted.  This prevented sockets
    from being untagged, which would result in cancelled requests stalling
    out with sub-optimal TCP ZeroWindow detection instead of sending RST
    packets.
    
    Add a comment explaining how this works.
    
    Switch from SO_ERROR (which has a side-effect) to SO_DEBUG (which doesn't).
    
    Add a unit test.
    
    Bug: 18706592
    
    (cherry picked from commit 4f263fdfcc0c9e3ebadeaa6717e93a130c09320c)
    
    Change-Id: I6746f4e0e5a6c9788cdcf35f74ccca5aa9a0e8b4

--- luni/src/test/java/libcore/java/io/FileDescriptorTest.java
+++ luni/src/test/java/libcore/java/io/FileDescriptorTest.java
@@ -17,14 +17,27 @@
 package libcore.java.io;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.net.ServerSocket;
 import junit.framework.TestCase;
 
 public class FileDescriptorTest extends TestCase {
-    public void testReadOnlyFileDescriptorSync() throws Exception {
-        File f= File.createTempFile("FileDescriptorTest", "tmp");
-        new RandomAccessFile(f, "r").getFD().sync();
-    }
+  public void testReadOnlyFileDescriptorSync() throws Exception {
+    File f= File.createTempFile("FileDescriptorTest", "tmp");
+    new RandomAccessFile(f, "r").getFD().sync();
+  }
+
+  public void test_isSocket() throws Exception {
+    File f = new File("/dev/null");
+    FileInputStream fis = new FileInputStream(f);
+    assertFalse(fis.getFD().isSocket());
+    fis.close();
+
+    ServerSocket s = new ServerSocket();
+    assertTrue(s.getImpl$().getFD$().isSocket());
+    s.close();
+  }
 }
