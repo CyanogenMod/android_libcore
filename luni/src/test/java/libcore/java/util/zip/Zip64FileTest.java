a1938dec320283543ee7faeb459a2fd372ceeb4e
Narayan Kamath <narayan@google.com>
2015-06-24 13:00:54 +0100

    zip64: Exercise getInputStream(ZipEntry) for files with > 65k entries.
    bug: https://code.google.com/p/android-developer-preview/issues/detail?id=2436
    Change-Id: I9243de6f73e14207b31818c5c8dc62e4225249c1

--- luni/src/test/java/libcore/java/util/zip/Zip64FileTest.java
+++ luni/src/test/java/libcore/java/util/zip/Zip64FileTest.java
@@ -16,10 +16,13 @@
 
 package libcore.java.util.zip;
 
+import libcore.io.Streams;
+
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
@@ -43,6 +46,16 @@ public final class Zip64FileTest extends AbstractZipFileTest {
             assertTrue(entries.hasMoreElements());
             ZipEntry ze = entries.nextElement();
             assertEquals(2, ze.getSize());
+            InputStream is = null;
+            try {
+                is = zf.getInputStream(ze);
+                byte[] uncompressed = Streams.readFully(is);
+                assertEquals(2, uncompressed.length);
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
         } finally {
             if (zf != null) {
                 zf.close();
