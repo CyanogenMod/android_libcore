cf8b6b44130216ba55589aceddb544b2390f4266
Narayan Kamath <narayan@google.com>
2015-08-07 09:55:41 +0100

    Revert "Revert "Cull unused Charset Encoders.""
    This reverts commit 7fc85ad29feb8719ebb6b5ac281cf4fb1a2241bb.

--- ojluni/src/main/java/java/nio/charset/Charset.java
+++ ojluni/src/main/java/java/nio/charset/Charset.java
@@ -45,7 +45,6 @@ import java.util.ServiceConfigurationError;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import sun.misc.ASCIICaseInsensitiveComparator;
-import sun.nio.cs.StandardCharsets;
 import sun.nio.cs.ThreadLocalCoders;
 import sun.security.action.GetPropertyAction;
 
