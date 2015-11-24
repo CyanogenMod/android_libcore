6537662798dba8569b299fbeeb47b21196398978
Przemyslaw Szczepaniak <pszczepaniak@google.com>
2015-11-24 15:30:06 +0000

    Add missing "java.version" system property.
    Fixes ibcore.java.lang.OldSystemTest#test_getProperties
    failure.
    
    Bug: 25861114
    Change-Id: I1e5e7b03c5d4368e9dc4b97d180fbc3befef47a1

--- ojluni/src/main/java/java/lang/System.java
+++ ojluni/src/main/java/java/lang/System.java
@@ -945,6 +945,7 @@ public final class System {
         p.put("java.class.version", "50.0");
         p.put("java.compiler", "");
         p.put("java.ext.dirs", "");
+        p.put("java.version", "0");
 
         // TODO: does this make any sense? Should we just leave java.home unset?
         String javaHome = getenv("JAVA_HOME");
