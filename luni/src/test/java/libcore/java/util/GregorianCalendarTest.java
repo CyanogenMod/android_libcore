dda702b5a424ec84623e1a1e285c3ba0d515c131
Paul Duffin <paulduffin@google.com>
2015-12-17 15:47:53 +0000

    Fix, expand and move test for GregorianCalendar change date
    The part of the test_getLeastMaximumI test that is broken is
    checking a fix that was added in
    https://issues.apache.org/jira/browse/HARMONY-2947. The fix
    aimed to make the method obey its contract and take into
    consideration the current values of the getFirstDayOfWeek,
    getMinimalDaysInFirstWeek, getGregorianChange and
    getTimeZone methods but it didn't.
    
    The test broken because OpenJDK considered TimeZone, and the
    test used the default time zone so it varied from run to run
    which explains why it worked in some cases but not in others.
    
    There are a number of other fields (apart from WEEK_OF_YEAR that
    are affected by the transition between Gregorian and Julian
    calendars so the part of the test that was broken was moved to
    libcore.java.util.GregorianCalendarTest, fixed to work properly
    by setting the TimeZone explicitly and expanded to test the
    other fields too.
    
    Bug: 25998114
    Change-Id: I4fb6ed6143c682fce6cd2029d09bd59b17b3541d

--- luni/src/test/java/libcore/java/util/GregorianCalendarTest.java
+++ luni/src/test/java/libcore/java/util/GregorianCalendarTest.java
@@ -18,6 +18,7 @@ package libcore.java.util;
 
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -26,6 +27,8 @@ import junit.framework.TestCase;
 public class GregorianCalendarTest extends TestCase {
 
     private static final TimeZone LOS_ANGELES = TimeZone.getTimeZone("America/Los_Angeles");
+
+    private static final TimeZone LONDON = TimeZone.getTimeZone("Europe/London");
 
     // Documented a previous difference in behavior between this and the RI, see
     // https://code.google.com/p/android/issues/detail?id=61993 for more details.
@@ -61,5 +64,69 @@ public class GregorianCalendarTest extends TestCase {
         greg.set(Calendar.DAY_OF_WEEK, differentDayOfWeek);
         assertEquals(differentWeekOfYear, greg.get(Calendar.WEEK_OF_YEAR));
         assertEquals(differentDayOfWeek, greg.get(Calendar.DAY_OF_WEEK));
+    }
+
+    /**
+     * Specialized tests for those fields affected by GregorianCalendar cut over date.
+     *
+     * <p>Expands on a regression test created for harmony-2947.
+     */
+    public void test_fieldsAffectedByGregorianCutOver() {
+
+        Date date = new Date(Date.parse("Jan 1 00:00:01 GMT 2000"));
+        assertEquals(946684801000L, date.getTime());
+
+        GregorianCalendar gc;
+
+        // Test in America/Los_Angeles
+        gc = new GregorianCalendar(LOS_ANGELES, Locale.ENGLISH);
+        gc.setGregorianChange(date);
+        gc.setTime(date);
+
+        // Check the date to ensure that it is 18th Dec 1999. The reason this is not 1st Jan 2000
+        // is that the offset for Los Angeles is GMT-08:00. setGregorianChange() is interpreted as
+        // a wall time, not an instant. So, the instant that corresponds to
+        // "1st Jan 2000 00:00:01 GMT" is 8 hours before the Julian/Gregorian switch in LA. The day
+        // before 1st Jan 2000 (Gregorian Calendar) would be 18th Dec 1999 in the Julian calendar.
+        // The reason it is not the 31st Dec 1999 is simply a result of the discontinuity that
+        // occurred when switching calendars. That happened for real when the calendars were
+        // switched in 1582.
+        //
+        // A different year explains why we get very different results for the methods being tested.
+        assertEquals(1999, gc.get(Calendar.YEAR));
+        assertEquals(Calendar.DECEMBER, gc.get(Calendar.MONTH));
+        assertEquals(18, gc.get(Calendar.DAY_OF_MONTH));
+
+        assertEquals(50, gc.getActualMaximum(Calendar.WEEK_OF_YEAR));
+        assertEquals(50, gc.getLeastMaximum(Calendar.WEEK_OF_YEAR));
+        assertEquals(3, gc.getActualMaximum(Calendar.WEEK_OF_MONTH));
+        assertEquals(3, gc.getLeastMaximum(Calendar.WEEK_OF_MONTH));
+        assertEquals(18, gc.getActualMaximum(Calendar.DAY_OF_MONTH));
+        assertEquals(18, gc.getLeastMaximum(Calendar.DAY_OF_MONTH));
+        assertEquals(352, gc.getActualMaximum(Calendar.DAY_OF_YEAR));
+        assertEquals(352, gc.getLeastMaximum(Calendar.DAY_OF_YEAR));
+        assertEquals(3, gc.getActualMaximum(Calendar.DAY_OF_WEEK_IN_MONTH));
+        assertEquals(3, gc.getLeastMaximum(Calendar.DAY_OF_WEEK_IN_MONTH));
+
+        // Test in Europe/London
+        gc = new GregorianCalendar(LONDON, Locale.ENGLISH);
+        gc.setGregorianChange(date);
+        gc.setTime(date);
+
+        // Check the date is actually 1st Jan 2000.
+        assertEquals(2000, gc.get(Calendar.YEAR));
+        assertEquals(Calendar.JANUARY, gc.get(Calendar.MONTH));
+        assertEquals(1, gc.get(Calendar.DAY_OF_MONTH));
+
+        assertEquals(53, gc.getActualMaximum(Calendar.WEEK_OF_YEAR));
+        assertEquals(52, gc.getLeastMaximum(Calendar.WEEK_OF_YEAR));
+        assertEquals(5, gc.getActualMaximum(Calendar.WEEK_OF_MONTH));
+        assertEquals(4, gc.getLeastMaximum(Calendar.WEEK_OF_MONTH));
+        assertEquals(31, gc.getActualMaximum(Calendar.DAY_OF_MONTH));
+        assertEquals(28, gc.getLeastMaximum(Calendar.DAY_OF_MONTH));
+        assertEquals(366, gc.getActualMaximum(Calendar.DAY_OF_YEAR));
+        assertEquals(365, gc.getLeastMaximum(Calendar.DAY_OF_YEAR));
+        assertEquals(5, gc.getActualMaximum(Calendar.DAY_OF_WEEK_IN_MONTH));
+        assertEquals(4, gc.getLeastMaximum(Calendar.DAY_OF_WEEK_IN_MONTH));
     }
 }
