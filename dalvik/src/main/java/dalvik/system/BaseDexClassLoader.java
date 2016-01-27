a3ed7c5ac9f007035fc7532db39bd342c75e1344
Todd Kennedy <toddke@google.com>
2016-01-27 08:30:03 -0800

    Modify dexpath dynamically
    In order to add splits without restarting the process, we need to dynamically
    modify the dex classpath. This isn't without its caveats; for example, if a
    split modifies a previously loaded class, the modified class won't be loaded
    until the process restarts.
    
    (cherry picked from commit 104fb10863f65fe04af2b40526d0d0abe3ba3e92)
    
    (cherry picked from commit 19916aa91deefffee7b3de3ef6b3df7e13f19d8a)
    
    Change-Id: I083e0fe80ec413963e0ad233341af5d886b7f9b5

--- dalvik/src/main/java/dalvik/system/BaseDexClassLoader.java
+++ dalvik/src/main/java/dalvik/system/BaseDexClassLoader.java
@@ -62,6 +62,13 @@ public class BaseDexClassLoader extends ClassLoader {
         return c;
     }
 
+    /**
+     * @hide
+     */
+    public void addDexPath(String dexPath) {
+        pathList.addDexPath(dexPath, null /*optimizedDirectory*/);
+    }
+
     @Override
     protected URL findResource(String name) {
         return pathList.findResource(name);
