multi-os-engine/libcore

Add OpenJDK 8 java.util.List Default Methods
and corresponding tests.

Added replaceAll and sort methods.

Also added forEach method along with the above two methods in
CopyOnWriteArrayList.

Bug: 27693350
Bug: 27540008

Change-Id: Id32df836e79bee614c0dce8c61dbc7b06731265d

luni/src/test/java/libcore/java/util/CollectionsTest.java
@@ -28,6 +28,8 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import junit.framework.TestCase;
 
+import util.ListDefaultMethodTester;
+
 public final class CollectionsTest extends TestCase {
 
     public void testEmptyEnumeration() {
@@ -543,5 +545,91 @@ public final class CollectionsTest extends TestCase {
         } catch (UnsupportedOperationException e) {
             // expected
         }
+    }
+
+    public void test_SynchronizedList_replaceAll() {
+        ListDefaultMethodTester.test_replaceAll(Collections.synchronizedList(new ArrayList<>()));
+    }
+
+    public void test_SynchronizedList_sort() {
+        ListDefaultMethodTester.test_sort(Collections.synchronizedList(new ArrayList<>()));
+    }
+
+    public void test_CheckedList_replaceAll() {
+        ListDefaultMethodTester.test_replaceAll(Collections.checkedList(new ArrayList<>(), Double.class));
+    }
+
+    public void test_CheckedList_sort() {
+        ListDefaultMethodTester.test_sort(Collections.checkedList(new ArrayList<>(), Double.class));
+    }
+
+    public void test_EmptyList_replaceAll() {
+        Collections.emptyList().replaceAll(k -> 1);
+
+        try {
+            Collections.emptyList().replaceAll(null);
+            fail();
+        } catch (NullPointerException e) {
+            // expected
+        }
+    }
+
+    public void test_EmptyList_sort() {
+        Collections.emptyList().sort((k1, k2) -> 1);
+    }
+
+    public void test_unmodifiableList_replaceAll() {
+        try {
+            Collections.unmodifiableList(new ArrayList<>()).replaceAll(k -> 1);
+            fail();
+        } catch (UnsupportedOperationException e) {
+            // expected
+        }
+
+        // with non empty list
+
+        try {
+            ArrayList l = new ArrayList();
+            l.add(1);
+            l.add(2);
+            Collections.unmodifiableList(l).replaceAll(k -> 1);
+            fail();
+        } catch (UnsupportedOperationException e) {
+            // expected
+        }
+    }
+
+    public void test_unmodifiableList_sort() {
+        try {
+            Collections.unmodifiableList(new ArrayList<>()).sort((k1, k2) -> 1);
+            fail();
+        } catch (UnsupportedOperationException e) {
+            // expected
+        }
+
+        // with non empty list
+
+        try {
+            ArrayList l = new ArrayList();
+            l.add(1);
+            l.add(2);
+            Collections.unmodifiableList(l).sort((k1, k2) -> 1);
+            fail();
+        } catch (UnsupportedOperationException e) {
+            // expected
+        }
+    }
+
+    public void test_SingletonList_replaceAll() {
+        try {
+            Collections.singletonList(1).replaceAll(k -> 2);
+            fail();
+        } catch (UnsupportedOperationException e) {
+            // expected
+        }
+    }
+
+    public void test_SingletonList_sort() {
+        Collections.singletonList(1).sort((k1, k2) -> 2);
     }
 }

CyanogenMod/android_libcore

Added replaceAll and sort methods from the upstream except for the
Added forEach method along with the above two methods in
(cherry-picked from commit 67b8c3f1414bf04e53c094b951c8ea7903998d90)

luni/src/test/java/libcore/java/util/CollectionsTest.java
@@ -511,4 +511,92 @@ public final class CollectionsTest extends TestCase {
         } catch (UnsupportedOperationException expected) {
         }
     }
+
+    public void test_SingletonMap_merge() {
+        try {
+            Collections.singletonMap(1, 11.0).merge(1, 5.0, (k, v) -> 5.0);
+            fail();
+        } catch (UnsupportedOperationException expected) {
+        }
+    }
+
+    public void test_SynchronizedList_replaceAll() {
+        ListDefaultMethodTester.test_replaceAll(Collections.synchronizedList(new ArrayList<>()));
+    }
+
+    public void test_SynchronizedList_sort() {
+        ListDefaultMethodTester.test_sort(Collections.synchronizedList(new ArrayList<>()));
+    }
+
+    public void test_CheckedList_replaceAll() {
+        ListDefaultMethodTester.test_replaceAll(Collections.checkedList(new ArrayList<>(), Integer.class));
+    }
+
+    public void test_CheckedList_sort() {
+        ListDefaultMethodTester.test_sort(Collections.checkedList(new ArrayList<>(), Integer.class));
+    }
+
+    public void test_EmptyList_replaceAll() {
+        Collections.emptyList().replaceAll(k -> 1);
+
+        try {
+            Collections.emptyList().replaceAll(null);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+    }
+
+    public void test_EmptyList_sort() {
+        Collections.emptyList().sort((k1, k2) -> 1);
+    }
+
+    public void test_unmodifiableList_replaceAll() {
+        try {
+            Collections.unmodifiableList(new ArrayList<>()).replaceAll(k -> 1);
+            fail();
+        } catch (UnsupportedOperationException expected) {
+        }
+
+        // with non empty list
+
+        try {
+            ArrayList l = new ArrayList();
+            l.add(1);
+            l.add(2);
+            Collections.unmodifiableList(l).replaceAll(k -> 1);
+            fail();
+        } catch (UnsupportedOperationException expected) {
+        }
+    }
+
+    public void test_unmodifiableList_sort() {
+        try {
+            Collections.unmodifiableList(new ArrayList<>()).sort((k1, k2) -> 1);
+            fail();
+        } catch (UnsupportedOperationException expected) {
+        }
+
+        // with non empty list
+
+        try {
+            ArrayList l = new ArrayList();
+            l.add(1);
+            l.add(2);
+            Collections.unmodifiableList(l).sort((k1, k2) -> 1);
+            fail();
+        } catch (UnsupportedOperationException expected) {
+        }
+    }
+
+    public void test_SingletonList_replaceAll() {
+        try {
+            Collections.singletonList(1).replaceAll(k -> 2);
+            fail();
+        } catch (UnsupportedOperationException expected) {
+        }
+    }
+
+    public void test_SingletonList_sort() {
+        Collections.singletonList(1).sort((k1, k2) -> 2);
+    }
 }