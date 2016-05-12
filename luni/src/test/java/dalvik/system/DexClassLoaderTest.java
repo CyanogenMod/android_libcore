multi-os-engine/libcore

Add CTS test for mixed Dex and Path class loader.
The test verifies you can use a DexClassLoader to generate optimized
code that will be picked up by a PathClassLoader.

Bug: 19937016
Change-Id: Ic977bd0db8275ef032d6e14c704f276d31cc6b1c

luni/src/test/java/dalvik/system/DexClassLoaderTest.java
@@ -393,4 +393,39 @@ public class DexClassLoaderTest extends TestCase {
     public void test_twoJar_diff_getResourceAsStream() throws Exception {
         createLoaderAndCallMethod("test.TestMethods", "test_diff_getResourceAsStream", jar1, jar2);
     }
+
+    /**
+     * Test that a DexClassLoader can be used to generate optimized code, then
+     * a subsequent PathClassLoader can be used to load the optimized code.
+     */
+    public void testDexThenPathClassLoader() throws Exception {
+      File dex = new File(srcDir, "loading-test-for-opt-test.dex");
+      copyResource("loading-test.dex", dex);
+
+      File odexdir = new File(new File(srcDir, "oat"), VMRuntime.getCurrentInstructionSet());
+      assertTrue(odexdir.mkdirs());
+
+      // Initially there should be no optimized code for dex file.
+      assertTrue(DexFile.isDexOptNeeded(dex.getAbsolutePath()));
+
+      // Executing code from the dex loader should cause optimized code to be
+      // generated.
+      DexClassLoader dexloader = new DexClassLoader(dex.getAbsolutePath(),
+          odexdir.getAbsolutePath(), null, ClassLoader.getSystemClassLoader());
+      Class c = dexloader.loadClass("test.Test1");
+      Method m = c.getMethod("test", (Class[]) null);
+      assertSame("blort", m.invoke(null, (Object[]) null));
+
+      // Move the generated code to the right location to be used by a
+      // PathClassLoader.
+      File oat = new File(odexdir, "loading-test-for-opt-test.dex");
+      File odex = new File(odexdir, "loading-test-for-opt-test.odex");
+      oat.renameTo(odex);
+
+      // Now there should be optimized code for the dex file.
+      assertFalse(DexFile.isDexOptNeeded(dex.getAbsolutePath()));
+
+      cleanUpDir(odexdir);
+      odexdir.delete();
+    }
 }

CyanogenMod/android_libcore

(cherry picked from commit e1815f2eeff5fd03d2b76d72d2c7a881911bccb7)
Change-Id: Ia7a7a19694f36fdf0824fe21c10d644cc04d1090

luni/src/test/java/dalvik/system/DexClassLoaderTest.java
@@ -70,7 +70,11 @@ public class DexClassLoaderTest extends TestCase {
         }
         File[] files = dir.listFiles();
         for (File file : files) {
-            assertTrue(file.delete());
+            if (file.isDirectory()) {
+                cleanUpDir(file);
+            } else {
+                assertTrue(file.delete());
+            }
         }
     }
 
@@ -392,5 +396,40 @@ public class DexClassLoaderTest extends TestCase {
      */
     public void test_twoJar_diff_getResourceAsStream() throws Exception {
         createLoaderAndCallMethod("test.TestMethods", "test_diff_getResourceAsStream", jar1, jar2);
+    }
+
+    /**
+     * Test that a DexClassLoader can be used to generate optimized code, then
+     * a subsequent PathClassLoader can be used to load the optimized code.
+     * (b/19937016).
+     */
+    public void testDexThenPathClassLoader() throws Exception {
+        // Use a DexClassLoader to create optimized code.
+        File dex = new File(srcDir, "dex-then-path.dex");
+        copyResource("loading-test.dex", dex);
+
+        File oatDir = new File(new File(srcDir, "oat"), VMRuntime.getCurrentInstructionSet());
+        assertTrue(oatDir.mkdirs());
+
+        DexClassLoader dexloader = new DexClassLoader(dex.getAbsolutePath(),
+                oatDir.getAbsolutePath(), null, ClassLoader.getSystemClassLoader());
+        Class c1 = dexloader.loadClass("test.Test1");
+        Method m1 = c1.getMethod("test", (Class[]) null);
+        assertSame("blort", m1.invoke(null, (Object[]) null));
+
+        // Move the optimized code to the right location to be used by a
+        // PathClassLoader.
+        File odexForDexClassLoader = new File(oatDir, "dex-then-path.dex");
+        File odexForPathClassLoader = new File(oatDir, "dex-then-path.odex");
+        assertTrue(DexFile.isDexOptNeeded(dex.getAbsolutePath()));
+        assertTrue(odexForDexClassLoader.renameTo(odexForPathClassLoader));
+        assertFalse(DexFile.isDexOptNeeded(dex.getAbsolutePath()));
+
+        // Use a PathClassLoader that loads and runs the optimized code.
+        PathClassLoader pathloader = new PathClassLoader(dex.getAbsolutePath(),
+                ClassLoader.getSystemClassLoader());
+        Class c2 = pathloader.loadClass("test.Test1");
+        Method m2 = c2.getMethod("test", (Class[]) null);
+        assertSame("blort", m2.invoke(null, (Object[]) null));
     }
 }