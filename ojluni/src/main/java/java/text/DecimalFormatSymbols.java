d7f14d4672ba2d07fd7dfa7a74fb31879cd14369
Raph Levien <raph@google.com>
2016-05-16 15:31:54 -0700

    Fix override of setCurrencySymbol in DecimalFormatSymbols
    The implementation of fromIcuInstance tries to restore currencySymbol
    from the ICU instance, but it's not effective because the subsequent
    setting of internationalCurrencySymbol overrides the value of
    currencySymbol. This patch just changes the order to avoid that.
    
    Also includes a test to verify that overriding the currency symbol in
    DecimalFormatSymbols is preserved in a roundtrip through the
    DecimalFormat constructor.
    
    Bug: 28732330
    Change-Id: I9961459d9c479975e60c305de37daca58effd3cf

--- ojluni/src/main/java/java/text/DecimalFormatSymbols.java
+++ ojluni/src/main/java/java/text/DecimalFormatSymbols.java
@@ -741,8 +741,8 @@ public class DecimalFormatSymbols implements Cloneable, Serializable {
             result.setCurrency(Currency.getInstance("XXX"));
         }
 
-        result.setCurrencySymbol(dfs.getCurrencySymbol());
         result.setInternationalCurrencySymbol(dfs.getInternationalCurrencySymbol());
+        result.setCurrencySymbol(dfs.getCurrencySymbol());
         return result;
     }
 
