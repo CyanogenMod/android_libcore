e03b33ae5072a36e38f2d4a02b4ceb92f29d167e
Narayan Kamath <narayan@google.com>
2016-05-20 11:37:54 +0100

    Observable: Call out to hasChanged() to figure out if something changes.
    bug: 28797950
    
    (cherry picked from commit fbabe73723c24c38cbfd6429387dae5e6478513f)
    
    Change-Id: I8dd1cccce31d440782a8b7a6210a89576febbdaa

--- ojluni/src/main/java/java/util/Observable.java
+++ ojluni/src/main/java/java/util/Observable.java
@@ -138,18 +138,19 @@ public class Observable {
 
         synchronized (this) {
             /* We don't want the Observer doing callbacks into
-             * arbitrary code while holding its own Monitor.
+             * arbitrary Observables while holding its own Monitor.
              * The code where we extract each Observable from
-             * the Vector and store the state of the Observer
+             * the ArrayList and store the state of the Observer
              * needs synchronization, but notifying observers
              * does not (should not).  The worst result of any
              * potential race-condition here is that:
+             *
              * 1) a newly-added Observer will miss a
              *   notification in progress
              * 2) a recently unregistered Observer will be
              *   wrongly notified when it doesn't care
              */
-            if (!changed)
+            if (!hasChanged())
                 return;
 
             arrLocal = observers.toArray(new Observer[observers.size()]);
