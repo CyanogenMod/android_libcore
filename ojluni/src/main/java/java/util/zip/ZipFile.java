d95a908045486d862dfe0be056dd4ecfb3bd7693
Przemyslaw Szczepaniak <pszczepaniak@google.com>
2016-08-25 15:50:13 +0100

    Fix ZipFile exception for non-existent files
    This change fixes N regression in java.util.zip.ZipFile constructor.
    In previous android releases, for non-existent files, ZipFile
    constructor was throwing FileNotFoundException. Due to
    change in commit adc40417389a57bc7a831f55334bffcc07343eb9,
    N started to use ZipException in this case. This change
    reverts to pre-N behavior.
    
    Test: libcore.java.util.zip.ZipFileTest
    Bug: 31077136
    Change-Id: I7c7240d0808552f4e0cf4ed4ccbda7537df2d2b1

--- ojluni/src/main/java/java/util/zip/ZipFile.java
+++ ojluni/src/main/java/java/util/zip/ZipFile.java
@@ -31,6 +31,7 @@ import java.io.InputStream;
 import java.io.IOException;
 import java.io.EOFException;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayDeque;
@@ -203,9 +204,15 @@ class ZipFile implements ZipConstants, Closeable {
                                                Integer.toHexString(mode));
         }
 
-        // Android-changed: Error out early if the file is too short.
-        if (file.length() < ZipConstants.ENDHDR) {
-            throw new ZipException("File too short to be a zip file: " + file.length());
+
+        // Android-changed: Error out early if the file is too short or non-existent.
+        long length = file.length();
+        if (length < ZipConstants.ENDHDR) {
+            if (length == 0 && !file.exists()) {
+                throw new FileNotFoundException("File doesn't exist: " + file);
+            } else {
+                throw new ZipException("File too short to be a zip file: " + file.length());
+            }
         }
         String name = file.getPath();
 
